632aaf6010a14b91504c44e69848a4ce7551b6f7
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-06 13:41:59 -0400

    Update 6 - funciones_parámetros.dart

--- 6 - funciones_parámetros.dart
+++ 6 - funciones_parámetros.dart
@@ -1,14 +1,19 @@
 void main() {
   print(greetEveryone());
   
-  print('Suma: ${addtwoNumbersOptional(10,20)}');
+  print('Suma: ${addTwoNumbersOptional(10, 20)}'); // Suma 10 y 20
+print('Suma: ${addTwoNumbersOptional(10)}'); // Suma 10 y 0 (por defecto)
 
+}
+
+addTwoNumbersOptional(int i, int j) {
+  return a + b;
 }
 
 String greetEveryone() => 'Hello everyone!';
 
-int addtwoNumbersOptional(int a, [int b = 0]){
-  b??=0;
+int addTwoNumbersOptional2(int a, [int b = 0]) {
+  b ??= 0; // Asegura que b tenga un valor
   return a + b;
 }
 
