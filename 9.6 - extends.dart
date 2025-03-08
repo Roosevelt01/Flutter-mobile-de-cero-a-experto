dff1d7fd66f152824b495eaf2267718a24ccc328
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-08 12:37:57 -0400

    Create 9.6 - extends.dart

---
+++ 9.6 - extends.dart
@@ -0,0 +1,38 @@
+
+double chargePhone(EnergyPlant plant){
+  if(plant.energyLeft < 10){
+    throw Exception('Not enough energy');
+  }
+  
+  return plant.energyLeft -10;
+}
+
+enum PlantType { nuclear, wind, water}
+
+abstract class EnergyPlant{
+  double energyLeft;
+  PlantType type;//nuclear, wind, water
+  
+  EnergyPlant({
+    required this.energyLeft,
+    required this.type
+  });
+  
+  void consumeEnergy(double amount); 
+}
+
+class WindPlant extends EnergyPlant{
+  WindPlant({ required double initialEnergy })
+    :super(energyLeft: initialEnergy, type: PlantType.wind);
+  
+    @override
+    void consumeEnergy(double amount){
+      energyLeft -= amount;
+    }
+}
+
+void main() {
+    final windPlant = WindPlant(initialEnergy: 100);  
+  
+    print('wind: ${chargePhone(windPlant)}');
+}
