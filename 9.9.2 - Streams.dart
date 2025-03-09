bcff33797f9698551f71e2a7a0cc07a408a244a0
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-09 03:55:04 -0400

    Update 9.9.2 - Streams.dart

--- 9.9.2 - Streams.dart
+++ 9.9.2 - Streams.dart
@@ -1,3 +1,5 @@
+//Ejemplo #1
+
 void main(){
   emitNumbers().listen((value){
     print('Stream value: $value');
@@ -9,3 +11,42 @@ Stream<int> emitNumbers(){
     return value;
   }).take(5);
 }
+
+//Ejemplo #2
+
+
+
+Future<int> sumarStream(Stream<int> stream) async {
+  var suma = 0;
+  await for (final valor in stream) {
+    suma += valor;
+  }
+  return suma;
+}
+
+Stream<int> generarNumeros() async* {
+  for (int i = 1; i <= 5; i++) {
+    await Future.delayed(Duration(milliseconds: 500)); // Simula un retraso
+    yield i; // Envía el número al Stream
+  }
+}
+
+void main() async {
+  final miStream = generarNumeros();
+  final resultado = await sumarStream(miStream);
+  print('La suma es: $resultado'); // Salida: La suma es: 15
+}
+
+//Ejemplo #3
+Stream<int> numeros = Stream.fromIterable([1, 2, 3, 4, 5]);
+
+void main() async {
+  numeros
+      .where((numero) => numero % 2 == 0) // Filtra números pares
+      .map((numero) => numero * 2) // Multiplica por 2
+      .listen((numero) {
+    print(numero); // Imprime: 4, 8, 10
+  });
+}
+
+
