78a0f639591a8a30e52a69d2f213d775c7a625a5
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-22 23:31:56 -0400

    Update counterFunctionsScreenState.dart

---
+++ 2 - Sección 2: Flutter: primeros pasos/5 - Widgets personalizados/counterFunctionsScreenState.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class CounterFunctionsScreen extends StatefulWidget {
+  const CounterFunctionsScreen({super.key});
+
+  @override
+  State<CounterFunctionsScreen> createState() => _CounterFunctionsScreenState();
+}
+
+class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Function Screen'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh_rounded),
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickCounter',
+              style: const TextStyle(
+                fontSize: 160,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+            Text(
+              'Click${clickCounter > 1 ? 's' : ''}',
+              style: const TextStyle(fontSize: 25),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CustomBotton(icon: Icons.refresh_rounded),
+          const SizedBox(height: 10),
+
+          CustomBotton(icon: Icons.exposure_minus_1_outlined),
+          const SizedBox(height: 10),
+
+          CustomBotton(icon: Icons.plus_one),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomBotton extends StatelessWidget {
+  final IconData icon;
+
+  const CustomBotton({super.key, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      shape: const StadiumBorder(),
+      onPressed: () {},
+      child: Icon(icon),
+    );
+  }
+}
