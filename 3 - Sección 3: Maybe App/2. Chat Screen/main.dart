cc24670560c219f31134e518337a260bcf9d7f27
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-03-28 16:56:35 -0400

    java

--- 3 - Sección 3: Maybe App/2. Chat Screen/main.dart
+++ 3 - Sección 3: Maybe App/2. Chat Screen/main.dart
@@ -1,28 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart'; // Importa el tema personalizado de la aplicación
+import 'package:yes_no_app/presentation/chat/chat_screen.dart'; // Importa la pantalla de chat
 
-void main() => runApp(const MyApp());
+void main() => runApp(const MyApp()); // Punto de entrada de la aplicación
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Yes No App',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 4).theme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: FilledButton.tonal(
-            onPressed: () {},
-            child: const Text('Click me')
-          ),
-        ),
-      ),
+    return MaterialApp( 
+      title: 'Yes No App', 
+      debugShowCheckedModeBanner: false, 
+      theme: AppTheme(selectedColor: 4).theme(), 
+      home: const ChatScreen(), // Establece ChatScreen como la pantalla principal
     );
   }
 }
