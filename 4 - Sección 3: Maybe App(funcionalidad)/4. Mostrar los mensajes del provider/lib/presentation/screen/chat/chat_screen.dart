ea064e289a43cf114a0cfe139e3365ef359502f8
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-04-13 19:55:01 -0400

    java

---
+++ 4 - Sección 3: Maybe App(funcionalidad)/4. Mostrar los mensajes del provider/lib/presentation/screen/chat/chat_screen.dart
@@ -0,0 +1,64 @@
+// Archivo: lib/presentation/screens/chat/chat_screen.dart
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzyDzGUOkGUhw1tLvonMzkSk4XSa4f4aVtmEo6fyKOSoKE4jFiUH0V5MeY_B2RZRCMuPI&usqp=CAU'),
+          ),
+        ),
+        title: const Text('Alejandro'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    /*Paso 1: Acceder al ChatProvider usando context.watch<ChatProvider>()*/
+    final chatProvider = context.watch<ChatProvider>();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                /*Paso 2: Usar la longitud de la lista de mensajes del Provider*/
+                itemCount: chatProvider.messages.length,
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  /*Paso 3: Obtener el mensaje actual del Provider*/
+                  final message = chatProvider.messages[index];
+                  /*Paso 4: Condicional para mostrar burbuja de mi mensaje o del otro*/
+                  return (message.fromWho == FromWho.hers)
+                      ? HerMessageBubble()
+                      : MyMessageBubble();
+                },
+              ),
+            ),
+            const MessageFieldBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
