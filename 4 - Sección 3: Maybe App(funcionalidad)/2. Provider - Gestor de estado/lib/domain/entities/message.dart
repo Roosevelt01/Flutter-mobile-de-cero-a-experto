24f01657267c3703bd16ba9ce644ceba7c5f87a1
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-04-13 12:15:06 -0400

    java

---
+++ 4 - Sección 3: Maybe App(funcionalidad)/2. Provider - Gestor de estado/lib/domain/entities/message.dart
@@ -0,0 +1,25 @@
+
+// Definición de la enumeración 'FromWho' para identificar al remitente del mensaje.
+enum FromWho {
+  me, // Indica que el mensaje fue enviado por el usuario actual.
+  hers, // Indica que el mensaje fue enviado por otra persona.
+}
+
+// Definición de la clase 'Message', que representa una unidad de mensaje en la aplicación.
+class Message {
+  // Texto del mensaje, obligatorio.
+  final String text;
+
+  // URL opcional de una imagen asociada al mensaje (por ejemplo, un meme). Puede ser nulo.
+  final String? imageUrl;
+
+  // Identifica quién envió el mensaje, utilizando la enumeración 'FromWho'.
+  final FromWho fromWho;
+
+  // Constructor con nombre para la clase 'Message'.
+  Message({
+    required this.text,
+    this.imageUrl,
+    required this.fromWho,
+  });
+}
