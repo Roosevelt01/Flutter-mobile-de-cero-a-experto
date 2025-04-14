743cf5b25f740467a59169900edf2ad918909ed0
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-04-14 04:51:40 -0400

    Update chat_provider.dart

--- 4 - Sección 3: Maybe App(funcionalidad)/7. Mover el Scroll al final/lib/presentation/provides/chat_provider.dart
+++ 4 - Sección 3: Maybe App(funcionalidad)/7. Mover el Scroll al final/lib/presentation/provides/chat_provider.dart
@@ -1,24 +1,19 @@
 // Archivo: lib/presentation/providers/chat_provider.dart
-
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
-class ChatProvider extends ChangeNotifier {
-  /*Paso 1: Crear una instancia de ScrollController*/
-  final ScrollController chatScrollController = ScrollController();
+class ChatProvider extends ChangeNotifier {  
+  final ScrollController chatScrollController = ScrollController();/*Paso 1: Crear una instancia de ScrollController*/
 
   List<Message> messages = [
     Message(text: 'Hola amor', fromWho: FromWho.me),
     Message(text: 'Ya llegaste del trabajo?', fromWho: FromWho.me),
   ];
-
-  /*Paso 4: Método para enviar un nuevo mensaje y mover el scroll al final*/
-  Future<void> sendMessage(String text) async {
-    /*Paso 5: Simular un pequeño delay para la sensación de envío*/
-    await (Future.delayed(const Duration(milliseconds: 100)));
-
-    /*Paso 6: Evitar enviar mensajes vacíos*/
-    if (text.isEmpty) return;
+  
+  Future<void> sendMessage(String text) async {/*Paso 4: Método para enviar un nuevo mensaje y mover el scroll al final*/
+    await (Future.delayed(const Duration(milliseconds: 100))); /*Paso 5: Simular un pequeño delay para la sensación de envío*/
+    
+    if (text.isEmpty) return;/*Paso 6: Evitar enviar mensajes vacíos*/
 
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messages.add(newMessage);
