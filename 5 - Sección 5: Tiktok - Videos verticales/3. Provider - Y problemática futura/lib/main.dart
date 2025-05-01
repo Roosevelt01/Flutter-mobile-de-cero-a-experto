d2e74964592a6bd273b7e89df4cc09c1075d4e60
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-01 12:24:23 -0400

    java

---
+++ 5 - Sección 5: Tiktok - Videos verticales/3. Provider - Y problemática futura/lib/main.dart
@@ -0,0 +1,29 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; // Importa el paquete provider
+import 'package:tiktok/presentation/providers/discover_provider.dart'; // Importa DiscoverProvider
+import 'package:tiktok/presentation/screens/discover/discover_screen.dart'; // Importa DiscoverScreen
+import 'package:tiktok/theme/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider( // Widget que permite proporcionar múltiples providers a sus hijos
+      providers: [
+        ChangeNotifierProvider(create: (_) => DiscoverProvider()), // Proporciona una instancia de DiscoverProvider
+      ],
+      child: MaterialApp(
+        title: 'TikTok',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().getTheme(),
+        home: const DiscoverScreen(), // Utiliza DiscoverScreen como la pantalla de inicio
+      ),
+    );
+  }
+}
