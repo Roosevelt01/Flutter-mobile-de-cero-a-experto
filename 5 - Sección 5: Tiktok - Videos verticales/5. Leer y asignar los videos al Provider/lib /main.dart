d225076d4f8c9b1f6fec7c9058938d24eefb3294
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-02 13:46:44 -0400

    java

---
+++ 5 - Sección 5: Tiktok - Videos verticales/5. Leer y asignar los videos al Provider/lib /main.dart
@@ -0,0 +1,21 @@
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          lazy: false, // Asegura que el provider se cree inmediatamente
+          create: (_) => DiscoverProvider()..loadNextPage(), // Crea el provider y llama a loadNextPage
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TikTok',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().getTheme(),
+        home: const DiscoverScreen(),
+      ),
+    );
+  }
+}
