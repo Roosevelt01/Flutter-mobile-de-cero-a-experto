8d753343d3064bb4285c5f88dfd60f245c6eb294
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-02 22:58:47 -0400

    java

---
+++ 5 - Sección 5: Tiktok - Videos verticales/6. CircularProgress y PageView/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok/domain/entities/video_post.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  final List<VideoPost> videos;
+
+  const VideoScrollableView({
+    super.key,
+    required this.videos,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      // Especifica que el desplazamiento del PageView será vertical.
+      scrollDirection: Axis.vertical, 
+      // Aplica un efecto de rebote cuando se alcanza el inicio o el final del desplazamiento.
+      physics: const BouncingScrollPhysics(), 
+      children: [ // Define la lista de widgets que se mostrarán como "páginas" en el PageView.
+        Container(color: Colors.red),
+        Container(color: Colors.blueAccent),
+        Container(color: Colors.teal),
+        Container(color: Colors.yellow),
+        Container(color: Colors.pink),
+        Container(color: Colors.deepPurple),
+      ],
+    );
+  }
+}
