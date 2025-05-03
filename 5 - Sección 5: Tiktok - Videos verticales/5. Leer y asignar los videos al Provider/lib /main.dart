e602e10c0f2e8898ed92b4e4f8aa8f81f7924189
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-02 22:31:09 -0400

    java

--- 5 - Sección 5: Tiktok - Videos verticales/5. Leer y asignar los videos al Provider/lib /main.dart
+++ 5 - Sección 5: Tiktok - Videos verticales/5. Leer y asignar los videos al Provider/lib /main.dart
@@ -19,3 +19,35 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const PageViewExampleApp());
+
+class PageViewExampleApp extends StatelessWidget {
+  const PageViewExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Ejemplo básico de PageView')),
+        body: const PageView(
+          children: <Widget>[
+            Center(
+              child: Text('Página 1', style: TextStyle(fontSize: 24, color: Colors.white)),
+            ),
+            Center(
+              child: Text('Página 2', style: TextStyle(fontSize: 24, color: Colors.white)),
+            ),
+            Center(
+              child: Text('Página 3', style: TextStyle(fontSize: 24, color: Colors.white)),
+            ),
+          ],
+          scrollDirection: Axis.horizontal, // Scroll horizontal por defecto
+        ),
+      ),
+    );
+  }
+}
