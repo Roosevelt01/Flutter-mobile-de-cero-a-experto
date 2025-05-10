870e8aaa2912fd8165c0668f40f49009bd0656dd
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-09 23:03:33 -0400

    java

---
+++ 6 - Sección 6: Conceptos de Clean Architecture - Datasources - Repositories/2. Implementaciones/lib/infrastruture/datasources/local_video_datasource_impl.dart
@@ -0,0 +1,12 @@
+// lib/infrastructure/datasources/local_video_datasource_impl.dart
+
+import 'package:tiktok/domain/datasource/video_post_datasource.dart';
+import 'package:tiktok/domain/entities/video_post.dart';
+import 'package:tiktok/insfrastructure/models/local_video_model.dart';
+import 'package:tiktok/shared/data/local_video_posts.dart'; // Importa los datos locales
+
+// Declara la clase e indica que implementa la interfaz del Dominio
+class LocalVideoDatasourceImpl implements VideoPostsDatasource {
+
+  // Los métodos requeridos por la interfaz se añadirán a continuación
+}
