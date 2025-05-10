6e961dc42b754bdf418675db9c2a1ea25f2b02ea
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-09 23:23:25 -0400

    java

--- 6 - Sección 6: Conceptos de Clean Architecture - Datasources - Repositories/2. Implementaciones/lib/infrastruture/datasources/local_video_datasource_impl.dart
+++ 6 - Sección 6: Conceptos de Clean Architecture - Datasources - Repositories/2. Implementaciones/lib/infrastruture/datasources/local_video_datasource_impl.dart
@@ -8,5 +8,26 @@ import 'package:tiktok/shared/data/local_video_posts.dart'; // Importa los datos
 // Declara la clase e indica que implementa la interfaz del Dominio
 class LocalVideoDatasourceImpl implements VideoPostsDatasource {
 
-  // Los métodos requeridos por la interfaz se añadirán a continuación
+  @override
+  Future<List<String>> getFavoriteVideosByUser(String userId) {
+    // Implementación pendiente (Paso 3)
+    throw UnimplementedError(); // Placeholder temporal
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) async {
+    // Simula un retardo asíncrono de 2 segundos.
+    // Esto representa el tiempo que tomaría obtener datos de una fuente externa.
+    await Future.delayed(const Duration(seconds: 2));
+
+      // Accede a la lista de datos locales 'videoPosts'
+    final List<VideoPost> newVideos = videoPosts.map(
+      // Para cada 'video' en la lista 'videoPosts' (que es un mapa):
+      // 1. Crea una instancia de LocalVideoModel usando el factory constructor fromJson.
+      // 2. Llama al método toEntity() de LocalVideoModel para convertirlo a una entidad VideoPost.
+      (video) => LocalVideoModel.fromJson(video).toEntity()
+    ).toList(); // Convierte el resultado del map (un Iterable) de nuevo a una List<VideoPost>.
+
+    throw UnimplementedError(); // Placeholder temporal
+  }
 }
