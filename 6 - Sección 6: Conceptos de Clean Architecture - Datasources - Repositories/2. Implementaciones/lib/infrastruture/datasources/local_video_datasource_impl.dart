20845f9cd8e9e15c75c863801e139bb643ddff79
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-10 13:19:15 -0400

    java

--- 6 - Sección 6: Conceptos de Clean Architecture - Datasources - Repositories/2. Implementaciones/lib/infrastruture/datasources/local_video_datasource_impl.dart
+++ 6 - Sección 6: Conceptos de Clean Architecture - Datasources - Repositories/2. Implementaciones/lib/infrastruture/datasources/local_video_datasource_impl.dart
@@ -6,4 +6,12 @@ import 'package:tiktok/insfrastructure/models/local_video_model.dart';
 import 'package:tiktok/shared/data/local_video_posts.dart';
 
 //Se declara una nueva clase llamada LocalVideoDatasourceImpl
-class LocalVideoDatasourceImpl implements VideoPostsDatasource {}
+class LocalVideoDatasourceImpl implements VideoPostsDatasource {
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userId) { 
+    // Método no implementado en esta versión
+    throw UnimplementedError();
+  }
+
+}
