fa1efd698ae61781382d383334c9f3afddb93615
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-28 00:21:50 -0400

    flutter

---
+++ 7 - Sección 7: Widgets app/3. Opciones de nuestra aplicación/lib/config/menu/menu_item.dart
@@ -0,0 +1,38 @@
+//Paso 1
+import 'package:flutter/material.dart';
+
+// Paso 1: Definición de la clase MenuItem
+class MenuItem{
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+   // Constructor constante para crear instancias inmutables de MenuItem
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+// Lista global de todos los elementos del menú de la aplicación
+const appMenuItems = <MenuItem>[
+  // Primer elemento del menú: Botones
+  MenuItem(
+    title: "Botones",
+    subTitle: "Varios botones en Flutter",
+    link: "/buttons",
+    icon: Icons.smart_button_outlined,
+  ),
+
+  // Segundo elemento del menú: Botones
+  MenuItem(
+    title: "Tarjetas",
+    subTitle: "Un contenedor estirilizado",
+    link: "/card",
+    icon: Icons.credit_card,
+  ),
+  
+];
