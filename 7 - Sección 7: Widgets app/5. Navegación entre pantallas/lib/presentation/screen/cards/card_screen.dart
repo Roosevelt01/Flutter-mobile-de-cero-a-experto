ad71d4db16080a7d6c1a8691978168dff2b9e668
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-29 23:01:27 -0400

    flutter

--- 7 - Sección 7: Widgets app/5. Navegación entre pantallas/lib/presentation/screen/cards/card_screen.dart
+++ 7 - Sección 7: Widgets app/5. Navegación entre pantallas/lib/presentation/screen/cards/card_screen.dart
@@ -1,15 +1,16 @@
-//Paso 2
 
 import 'package:flutter/material.dart';
 
+// Paso 1: Definición de la pantalla de botones
 class CardsScreen extends StatelessWidget {
+  // Constructor constante para una inicialización eficiente
   const CardsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cards Screen'),
+    return Scaffold(// Widget fundamental de Material Design para una estructura de pantalla
+      appBar: AppBar(// Barra superior de la aplicación
+        title: const Text('Cards Screen'),// Título de la AppBar
       ),
       body: const Placeholder(),
     );
