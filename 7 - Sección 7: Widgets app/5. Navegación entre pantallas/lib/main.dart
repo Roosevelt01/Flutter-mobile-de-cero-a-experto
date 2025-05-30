92dd772247c55633edeaad00d1ddace6dcb59b5b
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-05-29 23:31:16 -0400

    flutter

--- 7 - Sección 7: Widgets app/5. Navegación entre pantallas/lib/main.dart
+++ 7 - Sección 7: Widgets app/5. Navegación entre pantallas/lib/main.dart
@@ -19,8 +19,8 @@ class MainApp extends StatelessWidget {
       home: const HomeScreen(),
       // Paso 4: Definición de rutas nombradas
       routes: {
-        'buttons': (context) => const ButtonsScreen(),
-        'cards': (context) => const CardsScreen(),
+        'buttons': (context) => const ButtonsScreen(),// Ruta para la pantalla de botones
+        'cards': (context) => const CardsScreen(),// Ruta para la pantalla de tarjetas
       }
     );
   }
