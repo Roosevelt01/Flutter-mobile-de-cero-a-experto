b04655f1fcfffe459b5f0d30fc83906fd66b46d7
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-17 21:32:12 -0400

    java2

--- 7 - Sección 7: Widgets app/10. Cards/lib/presentation/screen/screens.dart
+++
@@ -1,3 +0,0 @@
-export 'package:widgets_app/presentation/screen/burttons/buttons_screen.dart';
-export 'package:widgets_app/presentation/screen/cards/card_screen.dart';
-export 'package:widgets_app/presentation/screen/home/home_screen.dart';
