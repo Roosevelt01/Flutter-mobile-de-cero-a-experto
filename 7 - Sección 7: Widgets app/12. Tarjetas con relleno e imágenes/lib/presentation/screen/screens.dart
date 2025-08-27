a32ecf202d02b3874de33ac8b06c3c42a86c5434
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-08-27 13:19:59 -0400

    java2

---
+++ 7 - Sección 7: Widgets app/12. Tarjetas con relleno e imágenes/lib/presentation/screen/screens.dart
@@ -0,0 +1,3 @@
+export 'package:widgets_app/presentation/screen/burttons/buttons_screen.dart';
+export 'package:widgets_app/presentation/screen/cards/card_screen.dart';
+export 'package:widgets_app/presentation/screen/home/home_screen.dart';
