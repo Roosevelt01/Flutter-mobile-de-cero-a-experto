9471427b13b474155e87a2fcd07b282659bd074f
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-03 16:04:36 -0400

    java2

---
+++ 008. Sección 10: Widgets app - Continuación/001. ProgressIndicators/lib/presentation/screen/progress/progress_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ProgressScreen extends StatelessWidget {
+  static const String name = 'progress_screen';
+
+  const ProgressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Progress Indicators'),
+      ),
+      body: const _ProgressView(),   
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+          children: const [
+            SizedBox(width: 30),
+            Text('Circular progress indicator'),
+            SizedBox(height: 10),
+            CircularProgressIndicator(strokeWidth: 2, backgroundColor: Colors.black45),
+            SizedBox(height: 20),
+            Text('Circular indicator controlado'),_ControlledProgressIndicator()
+          ],
+        )
+    );
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
