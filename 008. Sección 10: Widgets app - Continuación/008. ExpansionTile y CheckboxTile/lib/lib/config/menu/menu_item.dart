7acb55026e2b76c4ba811aaf0dc8eea34dabd5bb
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-22 12:15:31 -0400

    java2

---
+++ 008. Sección 10: Widgets app - Continuación/008. ExpansionTile y CheckboxTile/lib/lib/config/menu/menu_item.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/material.dart';
+
+class MenuItem{
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: "Botones",
+    subTitle: "Varios botones en Flutter",
+    link: "/buttons",
+    icon: Icons.smart_button_outlined,
+  ),
+  
+  MenuItem(
+    title: "Tarjetas",
+    subTitle: "Un contenedor estirilizado",
+    link: "/cards",
+    icon: Icons.credit_card,
+  ),
+
+   MenuItem(
+    title: "Progress Indicators",
+    subTitle: "Generales y controlados",
+    link: "/progress",
+    icon: Icons.refresh_rounded,
+  ),
+  
+  MenuItem(
+    title: "Progress dialog",
+    subTitle: "Indicadores en pantalla",
+    link: "/snackbar",
+    icon: Icons.info_outline,
+  ),
+
+  MenuItem(
+    title: "Animated container",
+    subTitle: "Stateful widget animado",
+    link: "/animated",
+    icon: Icons.check_box_outline_blank_rounded,
+  ),
+
+  //Añadir la Opción al Menú
+  MenuItem(
+    title: "UI Controls + Tiles",
+    subTitle: "Serie de controles de Flutter",
+    link: "/ui-controls",
+    icon: Icons.car_rental_outlined,
+  ),
+];
