b09d2614569da85dc9144464fb0fcd50e86be18d
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-23 00:26:01 -0400

    flutter

--- 008. Sección 10: Widgets app - Continuación/010. Finalizar tutorial/lib/presentation/screen/app_tutorial/app_tutorial_screen.dart
+++ 008. Sección 10: Widgets app - Continuación/010. Finalizar tutorial/lib/presentation/screen/app_tutorial/app_tutorial_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
-//Paso 1: Definir el Modelo de Datos y la Lista de Slides
-class SlideInfo{
+//Paso 1
+/*class SlideInfo{
   final String title;
   final String caption;
   final String imageUrl;
 
-  SlideInfo({required this.title, required this.caption,  required this.imageUrl, });
+  SlideInfo({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
 }
 
 final slides = <SlideInfo>[
@@ -27,6 +31,7 @@ final slides = <SlideInfo>[
     caption: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
     imageUrl: 'assets/images/3.png'
   ),
+
 ];
 
 class AppTutorialScreen extends StatelessWidget {
@@ -36,11 +41,396 @@ class AppTutorialScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: slides.map((slideData) => _Slide(
+          title: slideData.title,
+          caption: slideData.caption,
+          imageUrl: slideData.imageUrl,
+        )).toList(),
+      ),
+    );
   }
 }
 
-// Paso 2: Implementar el PageView Dinámico(Código final)
+class _Slide extends StatelessWidget {
+    final String title;
+    final String caption;
+    final String imageUrl;
+
+    const _Slide({
+      required this.title,
+      required this.caption,
+      required this.imageUrl,
+    });
+
+    @override
+    Widget build(BuildContext context) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Center(
+          child: Column(
+            children: [
+              Image(image: AssetImage(imageUrl)),
+            ],
+          ),
+        ),
+      );
+  }
+}*/
+
+//Paso 2
+/*class SlideInfo{
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+    title: 'Busca la comida',
+    caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+    imageUrl: 'assets/images/1.png'
+  ),
+
+  SlideInfo(
+    title: 'Entrega rápida',
+    caption: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
+    imageUrl: 'assets/images/2.png'
+  ),
+
+  SlideInfo(
+    title: 'Disfruta la comida',
+    caption: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
+    imageUrl: 'assets/images/3.png'
+  ),
+
+];
+
+class AppTutorialScreen extends StatelessWidget {
+  static const String name = 'tutorial_screen';
+
+  const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: slides.map((slideData) => _Slide(
+          title: slideData.title,
+          caption: slideData.caption,
+          imageUrl: slideData.imageUrl,
+        )).toList(),
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+    final String title;
+    final String caption;
+    final String imageUrl;
+
+    const _Slide({
+      required this.title,
+      required this.caption,
+      required this.imageUrl,
+    });
+
+    @override
+    Widget build(BuildContext context) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Center(
+          child: Column(
+            children: [
+              Image(image: AssetImage(imageUrl)),
+              //Paso 2.1
+              const SizedBox(height: 20),
+              Text(title),
+              const SizedBox(height: 10),
+              Text(caption),
+            ],
+          ),
+        ),
+      );
+  }
+}*/
+
+//Paso 3
+/*class SlideInfo{
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+    title: 'Busca la comida',
+    caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+    imageUrl: 'assets/images/1.png'
+  ),
+
+  SlideInfo(
+    title: 'Entrega rápida',
+    caption: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
+    imageUrl: 'assets/images/2.png'
+  ),
+
+  SlideInfo(
+    title: 'Disfruta la comida',
+    caption: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
+    imageUrl: 'assets/images/3.png'
+  ),
+
+];
+
+class AppTutorialScreen extends StatelessWidget {
+  static const String name = 'tutorial_screen';
+
+  const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: slides.map((slideData) => _Slide(
+          title: slideData.title,
+          caption: slideData.caption,
+          imageUrl: slideData.imageUrl,
+        )).toList(),
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+    final String title;
+    final String caption;
+    final String imageUrl;
+
+    const _Slide({
+      required this.title,
+      required this.caption,
+      required this.imageUrl,
+    });
+
+    @override
+    Widget build(BuildContext context) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,//Paso 3.1
+            crossAxisAlignment: CrossAxisAlignment.start,//Paso 3.2
+            children: [
+              Image(image: AssetImage(imageUrl)),
+              const SizedBox(height: 20),
+              Text(title),
+              const SizedBox(height: 10),
+              Text(caption),
+            ],
+          ),
+        ),
+      );
+  }
+}*/
+
+// Paso 4
+/*class SlideInfo{
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+    title: 'Busca la comida',
+    caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+    imageUrl: 'assets/images/1.png'
+  ),
+
+  SlideInfo(
+    title: 'Entrega rápida',
+    caption: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
+    imageUrl: 'assets/images/2.png'
+  ),
+
+  SlideInfo(
+    title: 'Disfruta la comida',
+    caption: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
+    imageUrl: 'assets/images/3.png'
+  ),
+
+];
+
+class AppTutorialScreen extends StatelessWidget {
+  static const String name = 'tutorial_screen';
+
+  const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: slides.map((slideData) => _Slide(
+          title: slideData.title,
+          caption: slideData.caption,
+          imageUrl: slideData.imageUrl,
+        )).toList(),
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+    final String title;
+    final String caption;
+    final String imageUrl;
+
+    const _Slide({
+      required this.title,
+      required this.caption,
+      required this.imageUrl,
+    });
+
+    @override
+    Widget build(BuildContext context) {
+      final titleStyle = Theme.of(context).textTheme.titleLarge;// Paso 4.1
+      final captionStyle = Theme.of(context).textTheme.bodyMedium;// Paso 4.2
+
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image(image: AssetImage(imageUrl)),
+              const SizedBox(height: 20),
+              Text(title, style: titleStyle),// Paso 4.3
+              const SizedBox(height: 10),
+              Text(caption, style: captionStyle),// Paso 4.4
+            ],
+          ),
+        ),
+      );
+  }
+}*/
+
+// Paso 5
+/*class SlideInfo{
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+    title: 'Busca la comida',
+    caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+    imageUrl: 'assets/images/1.png'
+  ),
+
+  SlideInfo(
+    title: 'Entrega rápida',
+    caption: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
+    imageUrl: 'assets/images/2.png'
+  ),
+
+  SlideInfo(
+    title: 'Disfruta la comida',
+    caption: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
+    imageUrl: 'assets/images/3.png'
+  ),
+
+];
+
+class AppTutorialScreen extends StatelessWidget {
+  static const String name = 'tutorial_screen';
+
+  const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,// Paso 5.1
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: slides.map((slideData) => _Slide(
+          title: slideData.title,
+          caption: slideData.caption,
+          imageUrl: slideData.imageUrl,
+        )).toList(),
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+    final String title;
+    final String caption;
+    final String imageUrl;
+
+    const _Slide({
+      required this.title,
+      required this.caption,
+      required this.imageUrl,
+    });
+
+    @override
+    Widget build(BuildContext context) {
+      final titleStyle = Theme.of(context).textTheme.titleLarge;
+      final captionStyle = Theme.of(context).textTheme.bodyMedium;
+
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image(image: AssetImage(imageUrl)),
+              const SizedBox(height: 20),
+              Text(title, style: titleStyle),
+              const SizedBox(height: 10),
+              Text(caption, style: captionStyle),
+            ],
+          ),
+        ),
+      );
+  }
+}*/
+
+// Paso 6
 class SlideInfo{
   final String title;
   final String caption;
@@ -82,20 +472,34 @@ class AppTutorialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //Paso 2.1: utilizamos PageView para generar sus hijos dinámicamente a partir de la lista slides
-      body: PageView(
-        physics: const BouncingScrollPhysics(),
-        children: slides.map((slideData) => _Slide(
-          title: slideData.title,
-          caption: slideData.caption,
-          imageUrl: slideData.imageUrl,
-        )).toList(),
+      backgroundColor: Colors.white,
+      //paso 6.1: Envolemos el PageView con un Stack
+      body: Stack(
+        children: [
+          PageView(
+            physics: const BouncingScrollPhysics(),
+            children: slides.map((slideData) => _Slide(
+              title: slideData.title,
+              caption: slideData.caption,
+              imageUrl: slideData.imageUrl,
+            )).toList(),
+          ),
+
+          //Paso 6.2: Agregamos el botón
+          Positioned(
+            right: 30,
+            top: 50,
+            child: TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Salir'),
+            ),
+          )
+        ],
       ),
     );
   }
 }
 
-//Paso 2.2: _Slide widget
 class _Slide extends StatelessWidget {
     final String title;
     final String caption;
@@ -109,6 +513,24 @@ class _Slide extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
-      return const Placeholder();
+      final titleStyle = Theme.of(context).textTheme.titleLarge;
+      final captionStyle = Theme.of(context).textTheme.bodyMedium;
+
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image(image: AssetImage(imageUrl)),
+              const SizedBox(height: 20),
+              Text(title, style: titleStyle),
+              const SizedBox(height: 10),
+              Text(caption, style: captionStyle),
+            ],
+          ),
+        ),
+      );
   }
 }
