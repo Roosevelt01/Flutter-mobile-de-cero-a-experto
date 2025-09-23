93e2ad476c957a8933e98efdc36623d770180427
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-22 21:30:46 -0400

    flutter

---
+++ 008. Sección 10: Widgets app - Continuación/009. PageView - App Tutorial/presentation/screen/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+//Paso 1
+/*class SlideInfo{
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+    title: 'Busca la comida',
+    caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+    imageUrl: 'assets/images/1.png'
+  ),
+
+  SlideInfo(
+    title: 'Entrega rápida',
+    caption: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
+    imageUrl: 'assets/images/2.png'
+  ),
+
+  SlideInfo(
+    title: 'Disfruta la comida',
+    caption: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
+    imageUrl: 'assets/images/3.png'
+  ),
+
+];
+
+class AppTutorialScreen extends StatelessWidget {
+  static const String name = 'tutorial_screen';
+
+  const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}*/
+
+//Paso 2
+class SlideInfo{
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+    title: 'Busca la comida',
+    caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+    imageUrl: 'assets/images/1.png'
+  ),
+
+  SlideInfo(
+    title: 'Entrega rápida',
+    caption: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
+    imageUrl: 'assets/images/2.png'
+  ),
+
+  SlideInfo(
+    title: 'Disfruta la comida',
+    caption: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
+    imageUrl: 'assets/images/3.png'
+  ),
+
+];
+
+class AppTutorialScreen extends StatelessWidget {
+  static const String name = 'tutorial_screen';
+
+  const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: slides.map((slideData) => _Slide(
+          title: slideData.title,
+          caption: slideData.caption,
+          imageUrl: slideData.imageUrl,
+        )).toList(),
+      ),
+    );
+  }
+}
+
+//2.1 Gemini agregale comentarios 
+class _Slide extends StatelessWidget {
+    final String title;
+    final String caption;
+    final String imageUrl;
+
+    const _Slide({
+      required this.title,
+      required this.caption,
+      required this.imageUrl,
+    });
+
+    @override
+    Widget build(BuildContext context) {
+      return const Placeholder();
+  }
+}
---
+++ 008. Sección 10: Widgets app - Continuación/009. PageView - App Tutorial/presentation/screen/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,324 @@
+import 'package:flutter/material.dart';
+
+//Paso 1: Añadir el ExpansionTile
+class UiControlsScreen extends StatelessWidget {
+  static const String name = 'ui_controls_screen';
+
+  const UiControlsScreen
+  ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UI Controls'),
+      ),
+      body: const _UiControlsViiew(),
+    );
+  }
+}
+
+class _UiControlsViiew extends StatefulWidget {
+  const _UiControlsViiew();
+
+  @override
+  State<_UiControlsViiew> createState() => _UiControlsViiewState();
+}
+
+enum Transportation{car, plane, boat, submarine}
+
+class _UiControlsViiewState extends State<_UiControlsViiew> {
+  
+  
+  bool isDeveloper = true;
+  Transportation selectedTransportation = Transportation.car;
+  
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+          SwitchListTile(
+            title: const Text('Developer Mode'), 
+            subtitle: const Text('Controles adicionales'),
+            value: isDeveloper,
+            onChanged: (value) => setState(() {
+              isDeveloper = !isDeveloper;
+            }),
+          ),
+
+          // Introduce un ExpansionTile en el ListView para que actúe como contenedor de los RadioListTile.
+          ExpansionTile(
+            title: const Text('Vehículo de transporte'),
+            subtitle: Text('$selectedTransportation'),
+          ),
+
+          RadioListTile(
+            title: const Text('By Car'), 
+            subtitle: const Text('Viajar por auto'),
+            value: Transportation.car, 
+            groupValue: selectedTransportation, 
+            onChanged: (value) => setState(() {
+              selectedTransportation = Transportation.car;
+            }),
+          ),
+
+          RadioListTile(
+            title: const Text('By boat'), 
+            subtitle: const Text('Viajar por barco'),
+            value: Transportation.boat, 
+            groupValue: selectedTransportation, 
+            onChanged: (value) => setState(() {
+              selectedTransportation = Transportation.boat;
+            }),
+          ),
+          
+          RadioListTile(
+            title: const Text('By plane'), 
+            subtitle: const Text('Viajar por avión'),
+            value: Transportation.plane, 
+            groupValue: selectedTransportation, 
+            onChanged: (value) => setState(() {
+              selectedTransportation = Transportation.plane;
+            }),
+          ),
+
+          RadioListTile(
+            title: const Text('By submarine'), 
+            subtitle: const Text('Viajar por submarino'),
+            value: Transportation.submarine, 
+            groupValue: selectedTransportation, 
+            onChanged: (value) => setState(() {
+              selectedTransportation = Transportation.submarine;
+            }),
+          ),
+
+      ],
+    );
+  }
+}
+
+//Paso 2: Mover los RadioListTile Hijos
+class UiControlsScreen extends StatelessWidget {
+  static const String name = 'ui_controls_screen';
+
+  const UiControlsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('UI Controls')),
+      body: const _UiControlsViiew(),
+    );
+  }
+}
+
+class _UiControlsViiew extends StatefulWidget {
+  const _UiControlsViiew();
+
+  @override
+  State<_UiControlsViiew> createState() => _UiControlsViiewState();
+}
+
+enum Transportation { car, plane, boat, submarine }
+
+class _UiControlsViiewState extends State<_UiControlsViiew> {
+  bool isDeveloper = true;
+  Transportation selectedTransportation = Transportation.car;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+        SwitchListTile(
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Controles adicionales'),
+          value: isDeveloper,
+          onChanged:
+              (value) => setState(() {
+                isDeveloper = !isDeveloper;
+              }),
+        ),
+
+        ExpansionTile(
+          title: const Text('Vehículo de transporte'),
+          subtitle: Text('$selectedTransportation'),
+          children: [// Los RadioListTile ahora están anidados aquí
+            //Corta todos los RadioListTile y pégalos dentro de la propiedad children del ExpansionTile.
+            RadioListTile(
+              title: const Text('By Car'),
+              subtitle: const Text('Viajar por auto'),
+              value: Transportation.car,
+              groupValue: selectedTransportation,
+              onChanged:
+                  (value) => setState(() {
+                    selectedTransportation = Transportation.car;
+                  }),
+            ),
+
+            RadioListTile(
+              title: const Text('By boat'),
+              subtitle: const Text('Viajar por barco'),
+              value: Transportation.boat,
+              groupValue: selectedTransportation,
+              onChanged:
+                  (value) => setState(() {
+                    selectedTransportation = Transportation.boat;
+                  }),
+            ),
+
+            RadioListTile(
+              title: const Text('By plane'),
+              subtitle: const Text('Viajar por avión'),
+              value: Transportation.plane,
+              groupValue: selectedTransportation,
+              onChanged:
+                  (value) => setState(() {
+                    selectedTransportation = Transportation.plane;
+                  }),
+            ),
+
+            RadioListTile(
+              title: const Text('By submarine'),
+              subtitle: const Text('Viajar por submarino'),
+              value: Transportation.submarine,
+              groupValue: selectedTransportation,
+              onChanged:
+                  (value) => setState(() {
+                    selectedTransportation = Transportation.submarine;
+                  }),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+//Paso 3: Definir el Estado y Añadir los Checkboxes(Código final)
+class UiControlsScreen extends StatelessWidget {
+  static const String name = 'ui_controls_screen';
+
+  const UiControlsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('UI Controls')),
+      body: const _UiControlsViiew(),
+    );
+  }
+}
+
+class _UiControlsViiew extends StatefulWidget {
+  const _UiControlsViiew();
+
+  @override
+  State<_UiControlsViiew> createState() => _UiControlsViiewState();
+}
+
+enum Transportation { car, plane, boat, submarine }
+
+class _UiControlsViiewState extends State<_UiControlsViiew> {
+  bool isDeveloper = true;
+  Transportation selectedTransportation = Transportation.car;
+  //Crea las variables de estado booleanas para las nuevas opciones
+  bool wantsBreakfast = false;
+  bool wantsLunch = false;
+  bool wantsDinner = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+        SwitchListTile(
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Controles adicionales'),
+          value: isDeveloper,
+          onChanged:
+              (value) => setState(() {
+                isDeveloper = !isDeveloper;
+              }),
+        ),
+
+        ExpansionTile(
+          title: const Text('Vehículo de transporte'),
+          subtitle: Text('$selectedTransportation'),
+          children: [
+            RadioListTile(
+              title: const Text('By Car'),
+              subtitle: const Text('Viajar por auto'),
+              value: Transportation.car,
+              groupValue: selectedTransportation,
+              onChanged:
+                  (value) => setState(() {
+                    selectedTransportation = Transportation.car;
+                  }),
+            ),
+
+            RadioListTile(
+              title: const Text('By boat'),
+              subtitle: const Text('Viajar por barco'),
+              value: Transportation.boat,
+              groupValue: selectedTransportation,
+              onChanged:
+                  (value) => setState(() {
+                    selectedTransportation = Transportation.boat;
+                  }),
+            ),
+
+            RadioListTile(
+              title: const Text('By plane'),
+              subtitle: const Text('Viajar por avión'),
+              value: Transportation.plane,
+              groupValue: selectedTransportation,
+              onChanged:
+                  (value) => setState(() {
+                    selectedTransportation = Transportation.plane;
+                  }),
+            ),
+
+            RadioListTile(
+              title: const Text('By submarine'),
+              subtitle: const Text('Viajar por submarino'),
+              value: Transportation.submarine,
+              groupValue: selectedTransportation,
+              onChanged:
+                  (value) => setState(() {
+                    selectedTransportation = Transportation.submarine;
+                  }),
+            ),
+          ],
+        ),
+      
+        // Se añade los CheckboxListTile correspondientes a la lista.
+        CheckboxListTile(
+          subtitle: const Text('¿Quieres desayunar?'),
+          value: wantsBreakfast,
+          onChanged: (value) => setState(() {
+            wantsBreakfast = !wantsBreakfast;
+          }),
+        ),
+        
+        CheckboxListTile(
+          subtitle: const Text('¿Quieres desayunar?'),
+          value: wantsLunch,
+          onChanged: (value) => setState(() {
+            wantsLunch = !wantsLunch;
+          }),
+        ),
+
+        CheckboxListTile(
+          subtitle: const Text('¿Quieres cenar?'),
+          value: wantsDinner,
+          onChanged: (value) => setState(() {
+            wantsDinner = !wantsDinner;
+          }),
+        ),
+                
+      ],
+    );
+  }
+}
