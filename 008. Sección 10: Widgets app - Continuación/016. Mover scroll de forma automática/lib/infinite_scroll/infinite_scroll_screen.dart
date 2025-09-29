36c7a0534fc1bf1e79a10319c1bc537b4ce4cccb
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-09-28 23:21:41 -0400

    flutter

--- 008. Sección 10: Widgets app - Continuación/016. Mover scroll de forma automática/lib/infinite_scroll/infinite_scroll_screen.dart
+++ 008. Sección 10: Widgets app - Continuación/016. Mover scroll de forma automática/lib/infinite_scroll/infinite_scroll_screen.dart
@@ -47,9 +47,6 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
 
     if (!isMounted) return; 
       setState(() {});     
-
-    movesScrollToBottom();//Paso 1.2: Mover el scroll hacia abajo
-
   }
 
   List<int> imagesIds = [1,2,3,4,5];
@@ -80,6 +77,131 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
       scrollController.position.pixels + 150, //Mover 120 pixeles
       duration: const Duration(milliseconds: 300), //Duracion de la animacion
       curve: Curves.fastOutSlowIn //Curva de la animacion
+    );
+  }
+
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1,2,3,4,5].map((e) => lastId + e));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: RefreshIndicator(
+          onRefresh: onRefresh,
+          edgeOffset: 30, 
+          strokeWidth: 2, 
+          child: ListView.builder(
+            controller: scrollController,
+            itemCount: imagesIds.length,
+            itemBuilder: (context, index) {
+              return FadeInImage(
+                fit: BoxFit.cover, 
+                width: double.infinity,
+                height: 300,
+                placeholder: const AssetImage('assets/images/jar-loading.gif'),
+                image: NetworkImage('https://picsum.photos/id/${imagesIds[index]}/500/300'),
+              );
+            },
+          ),
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pop(),
+        child: isLoading? SpinPerfect(
+          infinite: true, 
+          child: Icon(Icons.refresh_outlined),
+        ): 
+        FadeIn(child: Icon(Icons.arrow_back_ios_new_outlined)), 
+      )
+    );
+  }
+}
+
+// Paso 2: Integrar el Método en el Flujo de Carga
+class InfiniteScrollScreen extends StatefulWidget {
+  static const String name = 'infinite_screen';
+  
+  const InfiniteScrollScreen({super.key});
+
+  @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true; 
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      if ( (scrollController.position.pixels + 500) >= scrollController.position.maxScrollExtent ) {
+        loadNextPage(); 
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false; 
+    super.dispose();
+  }
+
+  Future loadNextPage() async {
+    if (isLoading) return; 
+    isLoading = true; 
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2)); 
+    
+    addFiveImages(); 
+    isLoading = false; 
+
+    if (!isMounted) return; 
+      setState(() {});     
+
+    movesScrollToBottom();//Paso 2.1: Mover el scroll hacia abajo
+
+  }
+
+  List<int> imagesIds = [1,2,3,4,5];
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {}); 
+
+    await Future.delayed(const Duration(seconds: 2)); 
+    if (!isMounted) return; 
+
+
+     isLoading = false;
+     final lastId = imagesIds.last; 
+     imagesIds.clear(); 
+     imagesIds.add(lastId + 1); 
+     addFiveImages(); 
+     
+     setState(() {}); 
+  }
+
+  void movesScrollToBottom() {
+    if (scrollController.position.pixels + 150 <= scrollController.position.maxScrollExtent) return;
+
+    scrollController.animateTo(
+      scrollController.position.pixels + 150, 
+      duration: const Duration(milliseconds: 300), 
+      curve: Curves.fastOutSlowIn 
     );
   }
 
