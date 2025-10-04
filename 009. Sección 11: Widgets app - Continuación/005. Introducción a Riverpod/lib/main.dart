bc3be7215a0bae6610060fe08699731f51d91b5e
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-10-04 14:28:21 -0400

    flutter

---
+++ 009. Sección 11: Widgets app - Continuación/005. Introducción a Riverpod/lib/main.dart
@@ -0,0 +1,27 @@
+
+//Paso 1: Configuración inicial para integrar Riverpod en la aplicación.
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; //Paso 1.1: Importamos el paquete de flutter_riverpod para la gestión de estado.
+import 'package:widgets_app/config/router/app_router.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+void main() { 
+  runApp(
+    //Paso 2.2: El `ProviderScope` es un widget que almacena el estado de nuestros providers. Es necesario envolver toda la aplicación con él para que Riverpod funcione.
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
+} 
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 1).getTheme(),
+    );
+  }
+}
