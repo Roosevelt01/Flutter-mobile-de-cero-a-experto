71e631f8f6ccc10a6836db6633a4ea236dd4dfe1
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-10-05 19:17:46 -0400

    flutter

--- 009. Sección 11: Widgets app - Continuación/006. Cambiar valores y modificadores/lib/presentation/screen/counter/counter_screen.dart
+++ 009. Sección 11: Widgets app - Continuación/006. Cambiar valores y modificadores/lib/presentation/screen/counter/counter_screen.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
-//Paso 1: Convertir el widget para que pueda consumir datos de Riverpod.
-
-//Paso 1.1: Se cambia `StatelessWidget` por `ConsumerWidget`. 
-//Este es un widget especial de Riverpod que nos permite "escuchar" los cambios en los providers.
-class CounterScreen extends ConsumerWidget { 
+//Paso 1: Para poder interactuar con los providers de Riverpod,
+//el widget debe heredar de `ConsumerWidget` en lugar de `StatelessWidget`.
+/*class CounterScreen extends ConsumerWidget { 
   
   static const name = 'counter_screen';
   
   const CounterScreen({super.key});
 
-  //Paso 1.2: El método `build` en un `ConsumerWidget` recibe un segundo argumento, el `WidgetRef ref`.
-  //`ref` es el objeto que nos permite interactuar con los providers (leer su estado, llamar métodos, etc.).
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    
+      final int clickCounter = ref.watch(counterProvider);
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
@@ -23,49 +23,69 @@ class CounterScreen extends ConsumerWidget {
       body: Center(
         child:
          Text(
-          "Valor 10", 
+          "Valor: $clickCounter", 
           style: Theme.of(context).textTheme.titleLarge,
           ),
       ),
 
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: (){}
+        onPressed: (){
+          //Paso 1.1: Para modificar el estado de un provider, usamos `ref.read`.
+          // `.notifier` nos da acceso al controlador del estado, y con `.state++`
+          // incrementamos su valor. Esto notifica a todos los widgets que estén
+          // observando (con `ref.watch`) para que se actualicen.
+          ref.read( counterProvider.notifier ).state++; 
+          //ref.read( counterProvider.notifier ).update( (state) => state++); Alternativa
+        }
       ),
     );
   }
-}
+}*/
 
-//Paso 2: Leer el Estado con ref.watch
-class CounterScreen extends ConsumerWidget { 
-  
+//Paso 2: Se integra la funcionalidad para cambiar el tema de la aplicación.
+class CounterScreen extends ConsumerWidget {
   static const name = 'counter_screen';
-  
+
   const CounterScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    
-    // Paso 2.1: Usamos `ref.watch` para obtener el valor del `counterProvider`.
-    // `watch` suscribe el widget a los cambios del provider. Cada vez que el valor
-    // del contador cambie, este widget se reconstruirá para mostrar el nuevo valor.
     final int clickCounter = ref.watch(counterProvider);
-    
+    //Paso 2.1: Observamos el estado del `isDarkModeProvider`.
+    // El widget se reconstruirá si el valor de `isDarkMode` cambia.
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+            //Paso 2.2: El ícono cambia dinámicamente dependiendo del valor de `isDarkMode`.
+            icon: Icon(
+              isDarkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined, //Paso 2.2:
+            ), 
+            onPressed: () {
+              //Paso 2.3: Usamos `ref.read` para leer el notifier del provider y cambiar su estado.
+              // Aquí, se invierte el valor booleano actual para alternar entre modo claro y oscuro.
+              ref.read(isDarkModeProvider.notifier).update((state) => !state);
+            },
+          ),
+        ],
       ),
       body: Center(
-        child:
-         Text(
-          "Valor: $clickCounter", // Paso 2.2: Mostramos el valor actual del provider en la UI.
+        child: Text(
+          "Valor: $clickCounter",
           style: Theme.of(context).textTheme.titleLarge,
-          ),
+        ),
       ),
 
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: (){}
+        child: Icon(Icons.add),
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+          //ref.read( counterProvider.notifier ).update( (state) => state++); Alternativa
+        },
       ),
     );
   }
