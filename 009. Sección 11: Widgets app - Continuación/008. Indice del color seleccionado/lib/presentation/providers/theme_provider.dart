23358e5ecc4eb22f02d7dc68612bf2f59592baba
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-10-06 19:27:45 -0400

    flutter

---
+++ 009. Sección 11: Widgets app - Continuación/008. Indice del color seleccionado/lib/presentation/providers/theme_provider.dart
@@ -0,0 +1,11 @@
+//Paso 1: Se crean los providers de Riverpod para gestionar el estado del tema.
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
+final isDarkModeProvider = StateProvider<bool>((ref) => false); 
+
+final colorListProvider = Provider((ref) => colorList);
+
+//Paso 1: Provider para mantener el estado del color seleccionado (el índice en la lista de colores).
+final selectedColorProvider = StateProvider((ref) => 0); 
