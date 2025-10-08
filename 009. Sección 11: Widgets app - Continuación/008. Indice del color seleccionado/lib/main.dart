28c8f0f1ca4fd4b8862c2d4f6e437dce239c741e
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-10-08 19:52:42 -0400

    flutter

--- 009. Sección 11: Widgets app - Continuación/008. Indice del color seleccionado/lib/main.dart
+++ 009. Sección 11: Widgets app - Continuación/008. Indice del color seleccionado/lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 import 'package:widgets_app/presentation/providers/theme_provider.dart';
-//Paso 1: Se envuelve la aplicación en un `ProviderScope` para que los providers de Riverpod estén disponibles en todo el árbol de widgets.
+
+//Paso 1: Se envuelve la aplicación en un `ProviderScope` para que los providers
+//de Riverpod estén disponibles en todo el árbol de widgets.
 void main() { 
   runApp(
     const ProviderScope(
@@ -19,14 +21,16 @@ class MainApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
 
-    //Paso 1.2: Se observa el estado de `isDarkModeProvider` y `selectedColorProvider` para reconstruir el widget cuando cambien.
+    //Paso 1.2: Se observa el estado de `isDarkModeProvider` y `selectedColorProvider` 
+    //para reconstruir el widget cuando cambien.
     final isDarkMode = ref.watch( isDarkModeProvider );
     final selectedColor = ref.watch( selectedColorProvider );
 
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      //Paso 1.3: Se crea una instancia de `AppTheme` con los valores actuales de color y modo oscuro, y se aplica el tema a la aplicación.
+      //Paso 1.3: Se crea una instancia de `AppTheme` con los valores actuales de color
+      // y modo oscuro, y se aplica el tema a la aplicación.
       theme: AppTheme(selectedColor: selectedColor, isDarkMode: isDarkMode).getTheme(),
     );
   }
