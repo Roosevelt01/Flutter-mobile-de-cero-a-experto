0c650bf6d6c26319241606bae24e658d2c9b24d6
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-10-09 09:26:12 -0400

    flutter

--- 009. Sección 11: Widgets app - Continuación/009. Riverpod - StateNotifier/lib/presentation/providers/theme_provider.dart
+++ 009. Sección 11: Widgets app - Continuación/009. Riverpod - StateNotifier/lib/presentation/providers/theme_provider.dart
@@ -12,14 +12,14 @@ final selectedColorProvider = StateProvider((ref) => 0);
 //Paso 1: Se crea un objeto de tipo AppTheme que es controlado por un Notifier.
 // Esto permite un manejo más centralizado y complejo del estado del tema.
 
-//Paso 1.1: Se define un `StateNotifierProvider` que crea una instancia de `ThemeNotifier`.
-// Este provider será el encargado de exponer el estado del tema (`AppTheme`) y permitir su modificación.
-final themeNotifiesProvider = StateNotifierProvider<ThemeNotifier, AppTheme>(
-  (ref) => ThemeNotifier());
-
-//Paso 1.2: Se define la clase `ThemeNotifier` que extiende `StateNotifier<AppTheme>`.
+//Paso 1.1: Se define la clase `ThemeNotifier` que extiende `StateNotifier<AppTheme>`.
 // Esta clase contendrá la lógica para manejar y actualizar el estado del tema de la aplicación.
 class ThemeNotifier extends StateNotifier<AppTheme> {
   // Se inicializa el estado con una instancia de `AppTheme()`, que contiene los valores por defecto.
   ThemeNotifier(): super(AppTheme());// STATE = new AppTheme();
 }
+
+//Paso 1.2: Se define un `StateNotifierProvider` que crea una instancia de `ThemeNotifier`.
+// Este provider será el encargado de exponer el estado del tema (`AppTheme`) y permitir su modificación.
+final themeNotifiesProvider = StateNotifierProvider<ThemeNotifier, AppTheme>(
+  (ref) => ThemeNotifier());
