920fc0a0a3e6785dbad50ed67d3410fe5bced525
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-10-14 22:39:39 -0400

    flutter

--- 010. Sección 12: Full App - Conemapedia/008. Realizar el mapeo de la respuesta de MovieDB/lib/config/infrastructure/mappers/movie_mapper.dart
+++ 010. Sección 12: Full App - Conemapedia/008. Realizar el mapeo de la respuesta de MovieDB/lib/config/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,3 @@
-
 import 'package:cinemapedia/config/infrastructure/models/moviedb/movie_moviedb.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
@@ -6,7 +5,6 @@ class MovieMapper {
   static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
 
     adult: moviedb.adult,
-    // Paso 4.1: Transforma el `backdropPath` para construir la URL completa. Si no hay imagen, usa una por defecto.
     backdropPath: (moviedb.backdropPath != '')
       ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
       : 'https://cdn.displate.com/artwork/270x380/2023-02-03/6b806b90ed460362ce845aec44991468_ee90576e764e6e2dc6be65372d967710.jpg',
@@ -16,10 +14,11 @@ class MovieMapper {
     originalTitle: moviedb.originalTitle,
     overview: moviedb.overview,
     popularity: moviedb.popularity,
-    // Paso 4.2: Transforma el `posterPath` para construir la URL completa. Si no hay imagen, usa una por defecto.
+
     posterPath: (moviedb.posterPath != '')
       ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
-      : 'https://cdn.displate.com/artwork/270x380/2023-02-03/6b806b90ed460362ce845aec44991468_ee90576e764e6e2dc6be65372d967710.jpg',
+      : 'no-poster', // Paso 1: Asigna un identificador para luego filtrar las películas que no tienen póster.
+    
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
     video: moviedb.video,
