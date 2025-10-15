25f6682a5dbf523fb9435bdaa1b80349cd147373
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-10-14 20:30:49 -0400

    flutter

--- 010. Sección 12: Full App - Conemapedia/007. MovieMappers - MovieDB hacia Movie Entity/lib/config/infrastructure/mappers/movie_mapper.dart
+++ 010. Sección 12: Full App - Conemapedia/007. MovieMappers - MovieDB hacia Movie Entity/lib/config/infrastructure/mappers/movie_mapper.dart
@@ -34,12 +34,12 @@ class MovieMapper {
   );
 }
 
-// Paso 5: Implementar la Lógica de Transformación de Imágenes(Código final)
+// Paso 4: Implementar la Lógica de Transformación de Imágenes(Código final)
 class MovieMapper {
   static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
 
     adult: moviedb.adult,
-    // Paso 5.1: Transforma el `backdropPath` para construir la URL completa. Si no hay imagen, usa una por defecto.
+    // Paso 4.1: Transforma el `backdropPath` para construir la URL completa. Si no hay imagen, usa una por defecto.
     backdropPath: (moviedb.backdropPath != '')
       ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
       : 'https://cdn.displate.com/artwork/270x380/2023-02-03/6b806b90ed460362ce845aec44991468_ee90576e764e6e2dc6be65372d967710.jpg',
@@ -49,7 +49,7 @@ class MovieMapper {
     originalTitle: moviedb.originalTitle,
     overview: moviedb.overview,
     popularity: moviedb.popularity,
-    // Paso 5.2: Transforma el `posterPath` para construir la URL completa. Si no hay imagen, usa una por defecto.
+    // Paso 4.2: Transforma el `posterPath` para construir la URL completa. Si no hay imagen, usa una por defecto.
     posterPath: (moviedb.posterPath != '')
       ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
       : 'https://cdn.displate.com/artwork/270x380/2023-02-03/6b806b90ed460362ce845aec44991468_ee90576e764e6e2dc6be65372d967710.jpg',
