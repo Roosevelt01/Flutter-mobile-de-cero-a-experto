b8cba856c09a27f927aa7a551052748ee347de65
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-10-20 13:11:46 -0400

    flutter

--- 010. Sección 12: Full App - Conemapedia/009. MovieRepository - Implementación/lib/config/infrastructure/repositores/movie_repository_impl.dart
+++ 010. Sección 12: Full App - Conemapedia/009. MovieRepository - Implementación/lib/config/infrastructure/repositores/movie_repository_impl.dart
@@ -1,14 +1,14 @@
 // Paso 1: Creación de la implementación del repositorio.
 
-/*import 'package:cinemapedia/domain/repositories/movies_repository.dart'; // 1. Importa el contrato (interfaz) del repositorio.
+import 'package:cinemapedia/domain/repositories/movies_repository.dart'; // 1. Importa el contrato (interfaz) del repositorio.
 
 // 1.1. Define la clase que implementa el contrato del repositorio.
 class MovieRepositoryImpl implements MoviesRepository {
 
-}*/
+}
 
 // Paso 2: Implementación de los métodos del contrato.
-/*import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/domain/repositories/movies_repository.dart';  // 2.1. Importaciones necesarias.
 
 class MovieRepositoryImpl implements MoviesRepository{
@@ -19,10 +19,10 @@ class MovieRepositoryImpl implements MoviesRepository{
     throw UnimplementedError();
   }
 
-}*/
+}
 
 // Paso 3: Inyección de la fuente de datos (Datasource).
-/*import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/domain/datasourve/movies_datasource.dart';
 import 'package:cinemapedia/domain/repositories/movies_repository.dart';  
 
@@ -35,9 +35,9 @@ class MovieRepositoryImpl implements MoviesRepository{
     // Aún falta llamar al datasource.
   }
 
-}*/
+}
 
-// Paso 4: Delegación de la llamada al Datasource.
+// Paso 4: Delegación de la llamada al Datasource.(Código final)
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/domain/datasourve/movies_datasource.dart';
 import 'package:cinemapedia/domain/repositories/movies_repository.dart';  
