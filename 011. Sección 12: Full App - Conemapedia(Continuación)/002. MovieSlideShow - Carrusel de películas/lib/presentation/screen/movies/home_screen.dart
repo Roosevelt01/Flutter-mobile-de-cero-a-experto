715144f0d4d6f1e04586121820f28fd116195347
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-10-25 21:14:08 -0400

    flutter

---
+++ 011. Sección 12: Full App - Conemapedia(Continuación)/002. MovieSlideShow - Carrusel de películas/lib/presentation/screen/movies/home_screen.dart
@@ -0,0 +1,47 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';//Paso 3.1: importar el CustomAppBar
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//Pas 1: Borramos el Expanded con el ListView y agregamos el MoviesSlideshow 
+class HomeScreen extends StatelessWidget {
+  static const String name = 'home-screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: _HomeView());
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    return Column(
+      children: [
+
+        const CustomAppBar(),
+
+        MoviesSlideshow( //Paso 3.2: Usar el MoviesSlideshow
+          movies: nowPlayingMovies,
+        ),
+      ],
+    );
+  }
+}
