0b8a0ef2c4c9c5bb2d118427ed424e23c6532bdb
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-10-28 10:50:44 -0400

    flutter

---
+++ 011. Sección 12: Full App - Conemapedia(Continuación)/006. CustomBottomNavigationBar/lib/presentation/screen/movies/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//Paso 1: Agregar el widget CustomBottomNavigation al HomeScreen
+class HomeScreen extends StatelessWidget {
+  static const String name = 'home-screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _HomeView(),
+      bottomNavigationBar: CustomBottomNavigation() // Agregamos la barra de navegación inferior
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch( moviesSlideshowProvider );
+
+    return Column(
+      children: [
+
+        const CustomAppBar(),
+
+        MoviesSlideshow( 
+          movies: nowPlayingMovies,
+        ),
+      ],
+    );
+  }
+}
---
+++ 011. Sección 12: Full App - Conemapedia(Continuación)/006. CustomBottomNavigationBar/lib/presentation/widgets/widgets.dart
@@ -0,0 +1,3 @@
+export 'shared/custom_appbar.dart';
+export 'movies/movies_slideshow.dart';
+export 'shared/custom_bottom_navigation.dart'; // Exportamos el nuevo widget de navegación inferior
