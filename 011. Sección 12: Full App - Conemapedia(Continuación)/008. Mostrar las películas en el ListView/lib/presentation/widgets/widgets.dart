65fd1212e181440b01ce13840cbfe7e4c38f73d2
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-11-06 11:16:36 -0400

    flutter

--- 011. Sección 12: Full App - Conemapedia(Continuación)/008. Mostrar las películas en el ListView/lib/presentation/widgets/widgets.dart
+++
@@ -1,4 +0,0 @@
-export 'movies/movies_slideshow.dart';
-export 'movies/movie_horizontal_listview.dart'; // Agregado exportación del MovieHorizontalListview
-export 'shared/custom_appbar.dart';
-export 'shared/custom_bottom_navigation.dart'; 
