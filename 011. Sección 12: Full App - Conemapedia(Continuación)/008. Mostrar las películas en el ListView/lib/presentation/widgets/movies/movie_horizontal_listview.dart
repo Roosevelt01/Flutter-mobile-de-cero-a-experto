a781b509a3db9d5bf9c63bfb586faaece796d8a7
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-11-06 12:20:26 -0400

    flutter

--- 011. Sección 12: Full App - Conemapedia(Continuación)/008. Mostrar las películas en el ListView/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ 011. Sección 12: Full App - Conemapedia(Continuación)/008. Mostrar las películas en el ListView/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -491,137 +491,6 @@ class MovieHorizontalListview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 250,
-      child: Column(
-        children: [
-          if (title != null || subTitle != null)
-            _Title(title: title, subTitle: subTitle),
-
-          Expanded(
-            child: ListView.builder(
-              itemCount: movies.length,
-              scrollDirection: Axis.horizontal,
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: (context, index) {
-                return _Slide(movie: movies[index]);
-              } 
-            )
-
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class _Slide extends StatelessWidget {
-  final Movie movie;
-
-  const _Slide({required this.movie});
-
-  @override
-  Widget build(BuildContext context) {
-
-    //Paso 6.1: 
-    final textStyle = Theme.of(context).textTheme;
-
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8),
-      child:Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-       children: [
-            SizedBox(
-              width: 100,
-              child: ClipRRect( 
-                borderRadius: BorderRadius.circular(20), 
-                child: Image.network( 
-                  movie.posterPath, 
-                  fit: BoxFit.cover, 
-                  width: 150,
-                  loadingBuilder: (context, child, loadingProgress) {
-                    if(loadingProgress != null){
-                      return const Padding( 
-                        padding: EdgeInsets.all(8.0), 
-                        child: Center(child: CircularProgressIndicator(strokeWidth: 2),)
-                      );
-                    }
-
-                    return FadeIn(child: child);
-                  },
-              ),
-              ),
-            ),
-
-            const SizedBox(height: 5),
-            
-            //Paso 6.2:
-            SizedBox(
-              width: 100,
-              child: Text(
-                movie.title,
-                maxLines: 2,
-                style: textStyle.titleSmall,
-              ),
-            ),
-        ],
-      ),
-    );
-  }
-}
-
-class _Title extends StatelessWidget {
-  final String? title;
-  final String? subTitle;
-
-  const _Title({this.title, this.subTitle});
-
-  @override
-  Widget build(BuildContext context) {
-    final titleStyle = Theme.of(context).textTheme.titleLarge;
-
-    return Container(
-      padding: const EdgeInsets.only(top: 0),
-      margin: const EdgeInsets.symmetric(horizontal: 10),
-      child: Row(
-        children: [
-          if (title != null) Text(title!, style: titleStyle),
-
-          const Spacer(),
-
-          if (subTitle != null)
-            FilledButton.tonal(
-              style: const ButtonStyle(visualDensity: VisualDensity.compact),
-              onPressed: () {},
-              child: Text(subTitle!),
-            ),
-        ],
-      ),
-    );
-  }
-}
-
-//Paso 7: 
-import 'package:animate_do/animate_do.dart';
-import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:flutter/material.dart';
-
-class MovieHorizontalListview extends StatelessWidget {
-  final List<Movie> movies;
-  final String? title;
-  final String? subTitle;
-  final VoidCallback? loadNextPage;
-
-  const MovieHorizontalListview({
-    super.key,
-    required this.movies,
-    this.title,
-    this.subTitle,
-    this.loadNextPage,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
       height: 250,      
       child: Column(
         children: [
@@ -662,7 +531,7 @@ class _Slide extends StatelessWidget {
        children: [
             SizedBox(
               width: 150,
-              height: 160,
+              height: 160, // Se añade una altura fija para la imagen
               child: ClipRRect( 
                 borderRadius: BorderRadius.circular(20), 
                 child: Image.network( 
@@ -683,8 +552,11 @@ class _Slide extends StatelessWidget {
               ),
             ),
             
+            const SizedBox(height: 5),
+
+            // Título de la película
             SizedBox(
-              width: 110,
+              width: 110, // Mismo ancho que la imagen
               child: Text(
                 movie.title,
                 maxLines: 1,
@@ -692,7 +564,7 @@ class _Slide extends StatelessWidget {
               ),
             ),
 
-            //Paso 6.1:
+            // Rating
             Row(
               children: [
                 Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
