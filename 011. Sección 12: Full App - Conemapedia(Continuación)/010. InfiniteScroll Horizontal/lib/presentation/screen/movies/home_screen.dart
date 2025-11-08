20bc11373cc75aba4f46b997f9db51ad3613ed4a
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-11-08 14:38:03 -0400

    flutter

---
+++ 011. Sección 12: Full App - Conemapedia(Continuación)/010. InfiniteScroll Horizontal/lib/presentation/screen/movies/home_screen.dart
@@ -0,0 +1,60 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String name = 'home-screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _HomeView(),
+      bottomNavigationBar: CustomBottomNavigation()
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+    final slideShowMovies = ref.watch( moviesSlideshowProvider ); 
+
+    return Column(
+      children: [
+
+        const CustomAppBar(),
+        
+        MoviesSlideshow( 
+          movies: slideShowMovies,
+        ),
+
+        MovieHorizontalListview(
+          movies: nowPlayingMovies,
+          title: 'En cines',
+          subTitle: 'Lunes 20',  
+          loadNextPage: () => ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage(), //Paso 1: Se pasa la función `loadNextPage` del provider para que el widget `MovieHorizontalListview` pueda cargar la siguiente página de películas cuando sea necesario.
+        )
+
+      ],
+    );
+  }
+}
