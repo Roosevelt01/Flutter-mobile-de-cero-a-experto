427efb7dddd62678e9a3efa54677c9c4a6d887d7
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-11-26 22:51:04 -0400

    flutter

---
+++ 011. Sección 12: Full App - Conemapedia(Continuación)/016. Escuchar múltiples providers simultáneamente/lib/presentation/providers/providers.dart
@@ -0,0 +1,4 @@
+export 'movies/initial_loading_provider.dart'; // Paso 1: Agregamos el provider initial_loading_provider.dart
+export 'movies/movies_providers.dart';
+export 'movies/movies_repository_provider.dart';
+export 'movies/movies_slideshow_provider.dart'; 
