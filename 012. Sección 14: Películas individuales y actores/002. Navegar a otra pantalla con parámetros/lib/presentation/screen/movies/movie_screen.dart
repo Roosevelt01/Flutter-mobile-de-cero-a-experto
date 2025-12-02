5fb5c8df0d50af64fae3be7688cad2af0f4dfac0
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-12-02 10:36:42 -0400

    flutter

---
+++ 012. Sección 14: Películas individuales y actores/002. Navegar a otra pantalla con parámetros/lib/presentation/screen/movies/movie_screen.dart
@@ -0,0 +1,43 @@
+//PAso 1: Crear la pantalla de peliculas y su estado
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatefulWidget {
+  const MovieScreen({super.key});
+
+  @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+//Paso 2: Agregar un parametro para el id de la pelicula
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatefulWidget {
+
+  //Paso 2.1: Definir una constante para el nombre de la pantalla
+  static const String name = 'movie_screen';
+
+  //Paso 2.2: Agregar un parametro para el id de la pelicula
+  final String movieId;
+
+  const MovieScreen({
+    super.key,
+    required this.movieId, //Paso 2.3: Hacer que el parametro sea requerido
+  });
+
+  @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
