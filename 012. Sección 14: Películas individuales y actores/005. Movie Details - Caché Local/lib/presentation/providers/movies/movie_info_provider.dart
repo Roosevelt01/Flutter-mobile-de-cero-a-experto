3c3d9e695df05b552083217400ddb7b52f556c7c
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-12-04 20:37:21 -0400

    flutter

--- 012. Sección 14: Películas individuales y actores/005. Movie Details - Caché Local/lib/presentation/providers/movies/movie_info_provider.dart
+++ 012. Sección 14: Películas individuales y actores/005. Movie Details - Caché Local/lib/presentation/providers/movies/movie_info_provider.dart
@@ -30,7 +30,7 @@ class MovieMapNotifier extends StateNotifier<Map<String, Movie>>{
   }): super({});
 }
 
-//Paso 4: Obtener Datos y Actualizar el Estado
+//Paso 4: Implementar la Verificación de Caché
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
@@ -51,79 +51,29 @@ class MovieMapNotifier extends StateNotifier<Map<String, Movie>>{
     }
 }
 
-
-
-
-
-
-//Paso 4: Agregamos la logica para cargar una pelicula solo si no esta en el estado
+//Paso 5: Obtener Datos y Actualizar el Estado
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
-class MovieMapNotifier extends StateNotifier<Map<String, Movie>>{
-    MovieMapNotifier(): super({});
-
-    Future<void> loadMovie(String movieId) async{
-      //Paso 4.1: Verificamos si la pelicula ya esta en el estado
-      if(state[movieId] != null) return;
-    }
-}
-
-//Paso 5: Agregamos la logica para obtener una pelicula usando una funcion externa
-import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:flutter_riverpod/legacy.dart';
-
-//Paso 5.1: Definimos un typedef para la funcion que obtiene una pelicula por su ID
 typedef GetMovieCallback = Future<Movie> Function(String movieId);
 
 class MovieMapNotifier extends StateNotifier<Map<String, Movie>>{
-    //Paso 5.2: Agregamos un parametro en el constructor para recibir la funcion
-    final GetMovieCallback getMovie;
-    
-    //Paso 5.3: Actualizamos el constructor para recibir la funcion
+    final GetMovieCallback getMovie; 
+
     MovieMapNotifier({
-      required this.getMovie
+      required this.getMovie, 
     }): super({});
 
     Future<void> loadMovie(String movieId) async{
       if(state[movieId] != null) return;
 
-      //Paso 5.4: Usamos la funcion para obtener la pelicula
-      final movie = await getMovie(movieId);
+      print('Realizando petición HTTP para: $movieId'); // Log para verificar que el caché funciona
+      
+      final movie = await getMovie(movieId); // Petición de red
 
-      //Paso 5.5: Actualizamos el estado con la nueva pelicula
-      state = { ...state, movieId: movie };
-    }
-}
-
-//Paso 6: Creamos el provider para el MovieMapNotifier (oódigo final)
-import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/presentation/providers/providers.dart';
-import 'package:flutter_riverpod/legacy.dart';
-
-//Paso 6.1: Definimos el provider para el MovieMapNotifier
-final movieInfoProvider = StateNotifierProvider((ref){
-  //Paso 6.2: Obtenemos el MovieRepository desde otro provider
-  final movieRepository  = ref.watch(movieRepositoryProvider);
-
-  //Paso 6.3: Retornamos una instancia de MovieMapNotifier
-  return MovieMapNotifier(getMovie: movieRepository.getMovieById);
-});
-
-typedef GetMovieCallback = Future<Movie> Function(String movieId);
-
-class MovieMapNotifier extends StateNotifier<Map<String, Movie>>{
-    final GetMovieCallback getMovie;
-    
-    MovieMapNotifier({
-      required this.getMovie
-    }): super({});
-
-    Future<void> loadMovie(String movieId) async{
-      if(state[movieId] != null) return;
-
-      final movie = await getMovie(movieId);
-
-      state = { ...state, movieId: movie };
+      // Actualización del Estado (Inmutable)
+      // Creamos un nuevo mapa con todas las películas anteriores (...state)
+      // y añadimos la nueva entrada (movieId: movie).
+      state = { ...state, movieId: movie };    
     }
 }
