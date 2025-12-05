ae1ba143712ad03a78b3d0bf9f3224a3db63ebeb
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-12-04 20:47:09 -0400

    flutter

--- 012. Sección 14: Películas individuales y actores/005. Movie Details - Caché Local/lib/presentation/providers/movies/movie_info_provider.dart
+++ 012. Sección 14: Películas individuales y actores/005. Movie Details - Caché Local/lib/presentation/providers/movies/movie_info_provider.dart
@@ -77,3 +77,35 @@ class MovieMapNotifier extends StateNotifier<Map<String, Movie>>{
       state = { ...state, movieId: movie };    
     }
 }
+
+//Paso 6: Creamos el provider para el MovieMapNotifier (oódigo final)
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+//Paso 6.1: Definimos el provider para el MovieMapNotifier
+final movieInfoProvider = StateNotifierProvider((ref){
+  //Paso 6.2: Obtenemos el MovieRepository desde otro provider
+  final movieRepository  = ref.watch(movieRepositoryProvider);
+
+  //Paso 6.3: Retornamos una instancia de MovieMapNotifier
+  return MovieMapNotifier(getMovie: movieRepository.getMovieById);
+});
+
+typedef GetMovieCallback = Future<Movie> Function(String movieId);
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>>{
+    final GetMovieCallback getMovie;
+    
+    MovieMapNotifier({
+      required this.getMovie
+    }):super({});
+
+    Future<void> loadMovie(String movieId) async{
+      if(state[movieId] != null) return;
+
+      final movie = await getMovie(movieId);
+
+      state = { ...state, movieId: movie };
+    }
+}
