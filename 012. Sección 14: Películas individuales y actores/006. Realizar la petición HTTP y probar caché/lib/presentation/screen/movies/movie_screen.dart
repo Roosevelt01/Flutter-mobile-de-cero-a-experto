897a007c2a4942ebeaf45c1bee905411094d8cdc
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-12-08 01:48:10 -0400

    flutter

--- 012. Sección 14: Películas individuales y actores/006. Realizar la petición HTTP y probar caché/lib/presentation/screen/movies/movie_screen.dart
+++ 012. Sección 14: Películas individuales y actores/006. Realizar la petición HTTP y probar caché/lib/presentation/screen/movies/movie_screen.dart
@@ -5,20 +5,18 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class MovieScreen extends ConsumerStatefulWidget {
 
   static const String name = 'movie_screen';
-
   final String movieId;
 
   const MovieScreen({
     super.key,
     required this.movieId, 
   });
-
   
   //Paso 1.1: Cambiamos State<MovieScreen> por MovieScreenState  
   
   //Antes de  la modificacion:
-  @override
-  State<MovieScreen> createState() => _MovieScreenState();*/
+  //@override
+  //State<MovieScreen> createState() => _MovieScreenState();*/
 
   //Despues de la modificacion:
   @override
@@ -32,7 +30,6 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     super.initState();
     
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -86,7 +83,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
       ),
     );
   }
-}*/
+}
 
 //Paso 3: Con la variable movie de tipo Movie? podemos acceder a los datos de la pelicula
 import 'package:cinemapedia/domain/entities/movie.dart';
@@ -130,49 +127,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
   }
 }
 
-//Paso 4: Con la variable movie de tipo Movie? podemos acceder a los datos de la pelicula
-import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-class MovieScreen extends ConsumerStatefulWidget {
-
-  static const String name = 'movie_screen';
-
-  final String movieId;
-
-  const MovieScreen({
-    super.key,
-    required this.movieId, 
-  });
-
-  @override
-  MovieScreenState createState() => MovieScreenState();
-}
-
-class MovieScreenState extends ConsumerState<MovieScreen> {
-  @override
-  void initState() {
-    super.initState();
-      ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    //Paso 3.1: creamos una variable movie de tipo Movie? para obtener la pelicula del Map
-    // es importante que sea Movie? porque puede ser null si no se ha cargado aun
-    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MovieID: ${widget.movieId}'), 
-      ),
-    );
-  }
-}
-
-//Paso 5: Mostramos un CircularProgressIndicator mientras se carga la pelicula(Código final)
+//Paso 4: Mostramos un CircularProgressIndicator mientras se carga la pelicula(Código final)
 import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -203,7 +158,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
   Widget build(BuildContext context) {
     final movie = ref.watch(movieInfoProvider)[widget.movieId];
 
-    //Paso 5.1: Mostramos un CircularProgressIndicator mientras se carga la pelicula
+    //Paso 4.1: Mostramos un CircularProgressIndicator mientras se carga la pelicula
     if (movie == null){
       return const Scaffold(
         body: Center(
