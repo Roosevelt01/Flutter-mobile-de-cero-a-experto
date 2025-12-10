334da3a972736ee656269256257b744a519f3cf6
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-12-09 21:24:32 -0400

    flutter

--- 012. Sección 14: Películas individuales y actores/007. Diseño de la pantalla de película/lib/presentation/screen/movies/movie_screen.dart
+++ 012. Sección 14: Películas individuales y actores/007. Diseño de la pantalla de película/lib/presentation/screen/movies/movie_screen.dart
@@ -441,101 +441,7 @@ class _CustomSliverAppBar extends StatelessWidget {
   }
 }
 
-// Paso 6: Agregamos el gradiente (sombra) inferior
-import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-class MovieScreen extends ConsumerStatefulWidget {
-  static const String name = 'movie_screen';
-  final String movieId;
-
-  const MovieScreen({
-    super.key,
-    required this.movieId, 
-  });
-
-  @override
-  MovieScreenState createState() => MovieScreenState();
-}
-
-class MovieScreenState extends ConsumerState<MovieScreen> {
-  @override
-  void initState() {
-    super.initState();
-    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final movie = ref.watch(movieInfoProvider)[widget.movieId];
-
-    if (movie == null){
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(strokeWidth: 2),
-        ),
-      );
-    }
-
-    return Scaffold(
-      body: CustomScrollView( 
-        physics: const ClampingScrollPhysics(), 
-        slivers: [
-          _CustomSliverAppBar(movie:movie) 
-        ],
-      ),
-    );
-  }
-}
-
-class _CustomSliverAppBar extends StatelessWidget {
-  final Movie movie;
-
-  const _CustomSliverAppBar({ required this.movie });
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size; 
-    
-    return SliverAppBar(
-      backgroundColor: Colors.black,
-      expandedHeight: size.height * 0.7, 
-      foregroundColor: Colors.white,
-      flexibleSpace: FlexibleSpaceBar(
-        background: Stack(
-          children: [
-            
-            SizedBox.expand(
-              child: Image.network(
-                movie.posterPath,  
-                fit: BoxFit.cover, 
-              ),
-            ),
-
-            const SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration( 
-                  gradient: LinearGradient( 
-                    begin: Alignment.topCenter, 
-                    end: Alignment.bottomCenter, 
-                    stops: [0.7, 1.0], 
-                    colors: [ 
-                      Colors.transparent, 
-                      Colors.black87      
-                    ]
-                  )
-                )
-              )
-            ),
-          ],
-        )
-      ),
-    );
-  }
-}
-
-// Paso 7: Agregamos el gradiente superior para la flecha de retroceso(Código final)
+// Paso 7: Degradado Superior para poder visualizar la barra de Estado(Código final)
 import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -623,7 +529,7 @@ class _CustomSliverAppBar extends StatelessWidget {
               )
             ),
 
-            //Paso 7.1: 
+            //Paso 7.1: Degradado Superior (Barra de Estado)
             const SizedBox.expand(
               child: DecoratedBox(
                 decoration: BoxDecoration( 
@@ -644,6 +550,7 @@ class _CustomSliverAppBar extends StatelessWidget {
                 )
               )
             ), //Cierre del segundo SizedBox.expand
+
           ],
         )
       ),
