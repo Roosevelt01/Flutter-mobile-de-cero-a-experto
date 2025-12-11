7a3523aa16b12144a1ea4cb1e29c33d92b7b3a7a
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-12-11 17:15:52 -0400

    flutter

---
+++ 012. Sección 14: Películas individuales y actores/009. Actores de la película/lib/domain/entities/actor.dart
@@ -0,0 +1,18 @@
+
+//Paso 1: Definición de la entidad Actor
+class Actor{
+  final String id;
+  final String name;
+  final String profilePath;
+  final String? character;
+
+  Actor({
+    required this.id,
+    required this.name,
+    required this.profilePath,
+    this.character,
+  });
+}
+
+// La clase Actor representa una entidad fundamental dentro del dominio de la aplicación.
+// Su función es encapsular los datos de un actor (como su ID, nombre, foto y personaje) de manera agnóstica a la fuente de datos (API), facilitando el uso de esta información en la interfaz de usuario y la lógica de negocio.
