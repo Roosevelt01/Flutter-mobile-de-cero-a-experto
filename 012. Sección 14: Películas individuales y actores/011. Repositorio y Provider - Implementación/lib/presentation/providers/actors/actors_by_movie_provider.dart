6700b01b4ae7824e435e66ff0071323dbe9da5bd
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-12-18 21:55:37 -0400

    flutter

--- 012. Sección 14: Películas individuales y actores/011. Repositorio y Provider - Implementación/lib/presentation/providers/actors/actors_by_movie_provider.dart
+++ 012. Sección 14: Películas individuales y actores/011. Repositorio y Provider - Implementación/lib/presentation/providers/actors/actors_by_movie_provider.dart
@@ -2,33 +2,35 @@ import 'package:cinemapedia/domain/entities/actor.dart';
 import 'package:cinemapedia/presentation/providers/actors/actors_resporitory_provider.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
+// 1. Definimos el Provider
 final actorsByMovieProvider = StateNotifierProvider<ActorsByMovieNotifier, Map<String, List<Actor>>>((ref){
+  
+  // Obtenemos la referencia al repositorio
   final actorsRepository  = ref.watch( actorsRepositoryProvider );
 
+  // Retornamos el Notifier inyectando la función del repositorio
   return ActorsByMovieNotifier(getActors: actorsRepository.getActorsByMovie);
 });
 
-/*
-  Quiero que tome en cuenta lo que está haciendo en este código
-
-  '505642': <Actor>[],
-  '505643': <Actor>[],
-  '505645': <Actor>[],
-  '501231': <Actor>[],
-*/
+// 2. Definimos el Callback (Firma de la función)
 typedef GetActorsCallback = Future<List<Actor>>Function(String movieId);
 
+// 3. Definimos el Notifier (Clase que maneja el estado)
 class ActorsByMovieNotifier extends StateNotifier<Map<String, List<Actor>>>{
     final GetActorsCallback getActors;
     
     ActorsByMovieNotifier({
       required this.getActors,
-    }): super({});
+    }): super({});// Estado inicial: Mapa vacío
 
     Future<void> loadActors(String movieId) async{
+      // Verificación de caché: Si ya existen actores para esta película, no hacemos nada
       if(state[movieId] != null) return;
+     
+      // Petición de datos
       final List<Actor> actors= await getActors(movieId);
 
+      // Actualización de estado (Crea un nuevo mapa con los datos anteriores + nuevos)
       state = { ...state, movieId: actors };
     }
 }
