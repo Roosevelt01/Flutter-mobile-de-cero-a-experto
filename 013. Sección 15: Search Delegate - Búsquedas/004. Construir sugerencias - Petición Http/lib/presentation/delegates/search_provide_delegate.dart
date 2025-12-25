39d82b7fc6c36c496db0672f1ccb96be7c48151c
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2025-12-25 02:18:01 -0400

    flutter

--- 013. Sección 15: Search Delegate - Búsquedas/004. Construir sugerencias - Petición Http/lib/presentation/delegates/search_provide_delegate.dart
+++ 013. Sección 15: Search Delegate - Búsquedas/004. Construir sugerencias - Petición Http/lib/presentation/delegates/search_provide_delegate.dart
@@ -1,113 +1,21 @@
-//Paso 1: Se agrega <Movie?> para indicar que el SearchDelegate puede devolver una Movie o null
-/*import 'package:cinemapedia/domain/entities/movie.dart'; // Importar la entidad Movie
-import 'package:flutter/material.dart'; 
-
-class SearchMovieDelegate extends SearchDelegate<Movie?> {
-  @override
-  String get searchFieldLabel => 'Buscar película';
-
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      const Text("buildActions"),
-    ];
-  }
-  
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return const Text("buildLeading");
-  }
-  
-  @override
-  Widget buildResults(BuildContext context) {
-    return const Text("buildResults");
-  }
-  
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return const Text("buildSuggestions");
-  }
-}*/
-
-//Paso 2: Modificamos el botón leading para cerrar el SearchDelegate
-/*import 'package:cinemapedia/domain/entities/movie.dart'; // Importar la entidad Movie
-import 'package:flutter/material.dart'; 
-
-class SearchMovieDelegate extends SearchDelegate<Movie?> {
-  @override
-  String get searchFieldLabel => 'Buscar película';
-
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      const Text("buildActions"),
-    ];
-  }
-  
-  @override
-  Widget? buildLeading(BuildContext context) {
-    //Paso 2.1: Modificar el botón leading para cerrar el SearchDelegate
-    return IconButton(
-      onPressed: () => close(context, null), //Cerrar el SearchDelegate y devolver null
-       icon: const Icon(Icons.arrow_back_ios_new_rounded) //Icono de flecha hacia atrás
-    );
-  }
-  
-  @override
-  Widget buildResults(BuildContext context) {
-    return const Text("buildResults");
-  }
-  
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return const Text("buildSuggestions");
-  }
-}*/
-
-//Paso 3: Agregamos un botón para limpiar la búsqueda
-/*import 'package:cinemapedia/domain/entities/movie.dart'; 
-import 'package:flutter/material.dart'; 
-
-class SearchMovieDelegate extends SearchDelegate<Movie?> {
-  @override
-  String get searchFieldLabel => 'Buscar película';
-
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    //Paso 3.1: Agregar un botón para limpiar la búsqueda
-    return [
-      IconButton(
-        onPressed: () => query = '', //Limpiar el texto de búsqueda
-        icon: const Icon( Icons.clear) //Icono de limpiar
-      ),
-    ];
-  }
-  
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () => close(context, null), 
-       icon: const Icon(Icons.arrow_back_ios_new_rounded) 
-    );
-  }
-  
-  @override
-  Widget buildResults(BuildContext context) {
-    return const Text("buildResults");
-  }
-  
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return const Text("buildSuggestions");
-  }
-}*/
-
-//Paso 4: Agregamos animaciones al botón de limpiar búsqueda
-import 'package:animate_do/animate_do.dart'; //Paso 4.1: Importar animate_do para usar animaciones
+//Paso 1: Con SearchMoviesCallback permitimos que el SearchDelegate pueda buscar películas usando un callback
+/*import 'package:animate_do/animate_do.dart'; 
 import 'package:cinemapedia/domain/entities/movie.dart'; 
 import 'package:flutter/material.dart'; 
 
+//Paso 1.1: Definir un  typedef para la función que buscará las películas
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
+ 
+  //Paso 1.2: Agregar el callback en el constructor
+  final SearchMoviesCallback searchMovies;
+
+  //Paso 1.3: Modificar el constructor para recibir el callback  
+  SearchMovieDelegate({
+    required this.searchMovies,
+  });
+
   @override
   String get searchFieldLabel => 'Buscar película';
 
@@ -116,9 +24,9 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
            
     return [
 
-        FadeIn( //Paso 4.3: Usar FadeIn para animar la aparición del botón
-          animate: query.isNotEmpty, //Solo animar si el query no está vacío
-          duration: const Duration(milliseconds: 200), //Duración de la animación
+        FadeIn( 
+          animate: query.isNotEmpty, 
+          duration: const Duration(milliseconds: 200), 
           child: IconButton(
             onPressed: () => query = '', 
             icon: const Icon( Icons.clear) 
@@ -144,5 +52,139 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   Widget buildSuggestions(BuildContext context) {
     return const Text("buildSuggestions");
+  }
+}*/
+
+//Paso 2: Modificar el SearchDelegate para usar el callback para buscar películas
+/*import 'package:animate_do/animate_do.dart'; 
+import 'package:cinemapedia/domain/entities/movie.dart'; 
+import 'package:flutter/material.dart'; 
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+ 
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({
+    required this.searchMovies,
+  });
+
+  @override
+  String get searchFieldLabel => 'Buscar película';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+           
+    return [
+
+        FadeIn( 
+          animate: query.isNotEmpty, 
+          duration: const Duration(milliseconds: 200), 
+          child: IconButton(
+            onPressed: () => query = '', 
+            icon: const Icon( Icons.clear) 
+          ),
+        ),
+      
+    ];
+  }
+  
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () => close(context, null), 
+       icon: const Icon(Icons.arrow_back_ios_new_rounded) 
+    );
+  }
+  
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text("buildResults");
+  }
+  
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    //Paso 2.1: Usar el callback para buscar películas
+    return FutureBuilder(
+      future: searchMovies(query), //Usamos el callback aquí para buscar películas
+      builder: (context, snapshot) {
+
+        return ListTile();
+      },
+    );
+  }
+}*/
+
+//Paso 3: Completar el FutureBuilder para mostrar las películas sugeridas
+import 'package:animate_do/animate_do.dart'; 
+import 'package:cinemapedia/domain/entities/movie.dart'; 
+import 'package:flutter/material.dart'; 
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+ 
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({
+    required this.searchMovies,
+  });
+
+  @override
+  String get searchFieldLabel => 'Buscar película';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+           
+    return [
+
+        FadeIn( 
+          animate: query.isNotEmpty, 
+          duration: const Duration(milliseconds: 200), 
+          child: IconButton(
+            onPressed: () => query = '', 
+            icon: const Icon( Icons.clear) 
+          ),
+        ),
+      
+    ];
+  }
+  
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () => close(context, null), 
+       icon: const Icon(Icons.arrow_back_ios_new_rounded) 
+    );
+  }
+  
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text("buildResults");
+  }
+  
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder(
+      future: searchMovies(query), 
+      builder: (context, snapshot) {
+
+        //Paso 3.1: Manejar los diferentes estados del FutureBuilder
+        final movies = snapshot.data ?? []; 
+
+        //Paso 3.2: Mostrar un indicador de carga mientras se obtienen los datos
+        return ListView.builder(
+          itemCount: movies.length, //Número de películas obtenidas para el query
+          itemBuilder: (context, index) {
+
+          final movie = movies[index]; //Obtener la película en la posición index para mostrarla
+          return ListTile( title: Text(movie.title)); //Mostrar el título de la película para cada item
+
+
+          }//Cierre itemBuilder,
+        );
+      },
+    );
   }
 }
---
+++ 013. Sección 15: Search Delegate - Búsquedas/004. Construir sugerencias - Petición Http/lib/presentation/widgets/shared/custom_appbar.dart
@@ -0,0 +1,103 @@
+//Paso 1: Cambiamos de StatelessWidget a ConsumerWidget para poder usar Riverpod
+/*import 'package:cinemapedia/presentation/delegates/search_provide_delegate.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // Paso 1.1: Importar flutter_riverpod
+
+//Paso 1.2: Usar ConsumerWidget en lugar de StatelessWidget
+class CustomAppBar extends ConsumerWidget {
+  const CustomAppBar({super.key});
+
+  @override //Paso 1.3: Agregar el parámetro ref en el método build
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    final titleStyle = Theme.of(context).textTheme.titleMedium;
+
+    return SafeArea(
+      bottom: false,
+        child: Padding(
+          padding : const EdgeInsets.symmetric(horizontal: 5),
+          child: SizedBox(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Icon(Icons.movie_outlined, color: colors.primary),
+                const SizedBox(width: 5),
+                Text('Cinemapedia', style: titleStyle?.copyWith(color: colors.primary) ),
+        
+                const Spacer(),
+        
+                 IconButton(onPressed: (){
+                    //Paso 1.2: Usar el showSearch para abrir el SearchDelegate
+                    showSearch(
+                      context: context, 
+                      delegate: SearchMovieDelegate()//Paso 1.3: Pasar el SearchDelegate
+                      );
+                 },
+
+                 icon: const Icon(Icons.search)
+                 )
+              ],
+            )
+          )
+        ),
+    );
+  }
+}*/
+
+//Paso 2: 
+import 'package:cinemapedia/presentation/delegates/search_provide_delegate.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; 
+
+
+class CustomAppBar extends ConsumerWidget {
+  const CustomAppBar({super.key});
+
+  @override 
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    final titleStyle = Theme.of(context).textTheme.titleMedium;
+
+    return SafeArea(
+      bottom: false,
+        child: Padding(
+          padding : const EdgeInsets.symmetric(horizontal: 5),
+          child: SizedBox(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Icon(Icons.movie_outlined, color: colors.primary),
+                const SizedBox(width: 5),
+                Text('Cinemapedia', style: titleStyle?.copyWith(color: colors.primary) ),
+        
+                const Spacer(),
+        
+                 IconButton(onPressed: (){
+
+                    //Paso 2.1: Leer el MovieRepository desde Riverpod    
+                    final movieRepository = ref.read(movieRepositoryProvider);  
+
+                    showSearch(
+                      context: context, 
+                      delegate: SearchMovieDelegate(
+                        //Paso 2.2: Pasar el callback que usa el MovieRepository para buscar películas
+                        searchMovies: movieRepository.searchMovies
+                      )
+                      );
+                 },
+
+                 icon: const Icon(Icons.search)
+                 )
+              ],
+            )
+          )
+        ),
+    );
+  }
+}
+
