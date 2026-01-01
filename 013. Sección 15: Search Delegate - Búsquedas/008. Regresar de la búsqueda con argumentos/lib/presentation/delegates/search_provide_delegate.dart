b163fef14ab976c1300b7821f70e93596d6179ac
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-01-01 19:43:57 -0400

    flutter

---
+++ 013. Sección 15: Search Delegate - Búsquedas/008. Regresar de la búsqueda con argumentos/lib/presentation/delegates/search_provide_delegate.dart
@@ -0,0 +1,278 @@
+//Paso 1: Agregamos la función onMovieSelected para manejar la selección de la película
+/*import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart'; 
+import 'package:cinemapedia/domain/entities/movie.dart'; 
+import 'package:flutter/material.dart'; 
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+ 
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({
+    required this.searchMovies,
+  });
+
+  @override
+  String get searchFieldLabel => 'Buscar película';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+           
+    return [
+
+        FadeIn( 
+          animate: query.isNotEmpty, 
+          duration: const Duration(milliseconds: 200), 
+          child: IconButton(
+            onPressed: () => query = '', 
+            icon: const Icon( Icons.clear) 
+          ),
+        ),
+    ];
+  }
+  
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () => close(context, null), 
+       icon: const Icon(Icons.arrow_back_ios_new_rounded) 
+    );
+  }
+  
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text("buildResults");
+  }
+  
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder(
+      future: searchMovies(query), 
+      builder: (context, snapshot) {
+
+        final movies = snapshot.data ?? []; 
+
+        return ListView.builder(
+          itemCount: movies.length, 
+          itemBuilder: (context, index) => _MovieItem(movie: movies[index]),
+        );
+      },
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  //Paso 1.1: Agregamos onMovieSelected para manejar la selección de la película
+  final Function onMovieSelected;
+
+  const _MovieItem({
+    required this.movie,
+    required this.onMovieSelected, //Paso 1.2: Agregamos onMovieSelected al constructor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme; 
+    final size = MediaQuery.of(context).size;
+   
+    //Paso 1.3: Usamos onMovieSelected en lugar de close directamente
+    return GestureDetector(
+      onTap: () => onMovieSelected(context, movie), // Llamamos a onMovieSelected al tocar el ítem
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          
+          child: Row(
+            children: [
+              
+              SizedBox(
+                width: size.width * 0.2,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20), 
+                  child: Image.network( movie.posterPath ), 
+                ),
+              ),
+      
+              const SizedBox(width: 10,), 
+      
+              SizedBox(
+                width: size.width * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    
+                    Text( 
+                          movie.title, 
+                          style: textStyle.titleMedium, 
+                        ),
+      
+                    (movie.overview.length > 100)
+                      ? Text( '${ movie.overview.substring(0, 100)}...' )
+                      : Text( movie.overview 
+                    ),
+                    
+                    Row(
+                      children: [
+                        Icon(Icons.star_half_rounded, color: Colors.yellow.shade800,),
+                        const SizedBox(width: 5,),
+                        
+                        Text( 
+                          HumanFormarts.number( movie.voteAverage,2),
+                          style: textStyle.bodyMedium!.copyWith(color: Colors.yellow.shade800),
+                        ),
+                      ],
+                    )
+      
+                  ],
+                ),
+              ),    
+            ],
+          ), 
+      
+      ),
+    );
+  }
+}*/
+
+//Paso 2: Modificar el código para pasar close como onMovieSelected
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart'; 
+import 'package:cinemapedia/domain/entities/movie.dart'; 
+import 'package:flutter/material.dart'; 
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+ 
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({
+    required this.searchMovies,
+  });
+
+  @override
+  String get searchFieldLabel => 'Buscar película';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+           
+    return [
+
+        FadeIn( 
+          animate: query.isNotEmpty, 
+          duration: const Duration(milliseconds: 200), 
+          child: IconButton(
+            onPressed: () => query = '', 
+            icon: const Icon( Icons.clear) 
+          ),
+        ),
+    ];
+  }
+  
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () => close(context, null), 
+       icon: const Icon(Icons.arrow_back_ios_new_rounded) 
+    );
+  }
+  
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text("buildResults");
+  }
+  
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder(
+      future: searchMovies(query), 
+      builder: (context, snapshot) {
+
+        final movies = snapshot.data ?? []; 
+
+        return ListView.builder(
+          itemCount: movies.length, 
+          itemBuilder: (context, index) => _MovieItem(
+            movie: movies[index],
+            onMovieSelected: close, //Paso 2.1: Pasamos close como onMovieSelected
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  final Function onMovieSelected;
+
+  const _MovieItem({
+    required this.movie,
+    required this.onMovieSelected, 
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme; 
+    final size = MediaQuery.of(context).size;
+   
+    return GestureDetector(
+      onTap: () => onMovieSelected(context, movie),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          
+          child: Row(
+            children: [
+              
+              SizedBox(
+                width: size.width * 0.2,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20), 
+                  child: Image.network( movie.posterPath ), 
+                ),
+              ),
+      
+              const SizedBox(width: 10,), 
+      
+              SizedBox(
+                width: size.width * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    
+                    Text( 
+                          movie.title, 
+                          style: textStyle.titleMedium, 
+                        ),
+      
+                    (movie.overview.length > 100)
+                      ? Text( '${ movie.overview.substring(0, 100)}...' )
+                      : Text( movie.overview 
+                    ),
+                    
+                    Row(
+                      children: [
+                        Icon(Icons.star_half_rounded, color: Colors.yellow.shade800,),
+                        const SizedBox(width: 5,),
+                        
+                        Text( 
+                          HumanFormarts.number( movie.voteAverage,2),
+                          style: textStyle.bodyMedium!.copyWith(color: Colors.yellow.shade800),
+                        ),
+                      ],
+                    )
+      
+                  ],
+                ),
+              ),    
+            ],
+          ), 
+      
+      ),
+    );
+  }
+}
---
+++ 013. Sección 15: Search Delegate - Búsquedas/008. Regresar de la búsqueda con argumentos/lib/presentation/shared/custom_appbar.dart
@@ -0,0 +1,119 @@
+//Paso 1: Modificamos el IconButton para que abra el SearchDelegate y capture la película seleccionada
+/*import 'package:cinemapedia/domain/entities/movie.dart'; // Importamos la entidad Movie
+import 'package:cinemapedia/presentation/delegates/search_provide_delegate.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; 
+
+class CustomAppBar extends ConsumerWidget {
+  const CustomAppBar({super.key});
+
+  @override 
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    final titleStyle = Theme.of(context).textTheme.titleMedium;
+
+    return SafeArea(
+      bottom: false,
+        child: Padding(
+          padding : const EdgeInsets.symmetric(horizontal: 5),
+          child: SizedBox(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Icon(Icons.movie_outlined, color: colors.primary),
+                const SizedBox(width: 5),
+                Text('Cinemapedia', style: titleStyle?.copyWith(color: colors.primary) ),
+        
+                const Spacer(),
+                
+                 //Paso 1.1: Agregamos el async a la función onPressed 
+                 IconButton(onPressed: () async {
+
+                    final movieRepository = ref.read(movieRepositoryProvider);  
+
+                    //Paso 1.2: Creamos la variable movie para capturar el resultado del showSearch
+                    //También se agrega <Movie?> para indicar que puede regresar un Movie o null
+                    final movie = await showSearch<Movie?> (
+                      context: context, 
+                      delegate: SearchMovieDelegate(
+                        searchMovies: movieRepository.searchMovies
+                      )
+                    );
+
+                    //Paso 1.3: Imprimimos en consola la película seleccionada
+                    print('Movie seleccionada: ${movie?.title}');
+                 },
+
+                 icon: const Icon(Icons.search)
+                 )
+              ],
+            )
+          )
+        ),
+    );
+  }
+}*/
+
+//Paso 2: 
+import 'package:cinemapedia/domain/entities/movie.dart'; 
+import 'package:cinemapedia/presentation/delegates/search_provide_delegate.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart'; 
+
+class CustomAppBar extends ConsumerWidget {
+  const CustomAppBar({super.key});
+
+  @override 
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    final titleStyle = Theme.of(context).textTheme.titleMedium;
+
+    return SafeArea(
+      bottom: false,
+        child: Padding(
+          padding : const EdgeInsets.symmetric(horizontal: 5),
+          child: SizedBox(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Icon(Icons.movie_outlined, color: colors.primary),
+                const SizedBox(width: 5),
+                Text('Cinemapedia', style: titleStyle?.copyWith(color: colors.primary) ),
+        
+                const Spacer(),
+                
+                //Paso 2.1: Modificamos el onPressed para usar .then en lugar de async/await
+                 IconButton(onPressed: (){
+
+                    final movieRepository = ref.read(movieRepositoryProvider);  
+                    
+                    showSearch<Movie?>(
+                      context: context, 
+                      delegate: SearchMovieDelegate(
+                        searchMovies: movieRepository.searchMovies
+                      )
+                    ).then((movie) { // Usamos .then para capturar el resultado
+                         if (movie == null) return; // Si es null no hacemos nada
+
+                          // Navegamos a la página de detalles usando go_router
+                         context.push('/movies/${ movie.id }'); 
+                    });
+
+                 },
+
+                 icon: const Icon(Icons.search)
+                 )
+              ],
+            )
+          )
+        ),
+    );
+  }
+}
