56618e87484726dfef753802a8b2f5a4bd0db96d
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-01-08 20:41:05 -0400

    flutter

--- 013. Sección 15: Search Delegate - Búsquedas/011. Search Movies Providers/lib/presentation/providers/search/search_movies_provider.dart
+++ 013. Sección 15: Search Delegate - Búsquedas/011. Search Movies Providers/lib/presentation/providers/search/search_movies_provider.dart
@@ -1,11 +1,9 @@
 //Paso 1: Modificamos el showSearch para que inicie con un valor de búsqueda
-/*
+
 import 'package:flutter_riverpod/legacy.dart'; // Importamos Riverpod
 
 // Definimos un StateProvider para almacenar el valor de búsqueda inicial
 final searchQueryProvider = StateProvider<String>((ref) => '');
-
-*/
 
 //Paso 2: En esta ocasión, probamos con "Batman" como valor inicial de búsqueda
 
