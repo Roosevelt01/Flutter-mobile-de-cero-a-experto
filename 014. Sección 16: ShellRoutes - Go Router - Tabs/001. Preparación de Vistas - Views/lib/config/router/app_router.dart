68849b06f65baeb6ed03705b54c5e91503d06dd9
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-01-22 10:56:55 -0400

    flutter

---
+++ 014. Sección 16: ShellRoutes - Go Router - Tabs/001. Preparación de Vistas - Views/lib/config/router/app_router.dart
@@ -0,0 +1,26 @@
+import 'package:cinemapedia/presentation/Views/views.dart';
+import 'package:cinemapedia/presentation/screen/movies/home_screen.dart';
+import 'package:cinemapedia/presentation/screen/movies/movie_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+
+    GoRoute(
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen( childView: HomeView(), ), // Agregando HomeView como childView
+    ),
+
+    GoRoute(
+      path: '/movie/:id', 
+      name: MovieScreen.name, 
+      builder: (context, state){
+        final movieId = state.pathParameters['id'] ?? 'no-id'; 
+
+        return MovieScreen(movieId: movieId); 
+      },
+    ),
+  ],
+);
--- 014. Sección 16: ShellRoutes - Go Router - Tabs/001. Preparación de Vistas - Views/lib/presentation/providers/actors/actors_resporitory_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cinemapedia/infrastructure/datasources/actor_moviedeb_datasource.dart';
-import 'package:cinemapedia/infrastructure/repositores/actor_repository_impl.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final actorsRepositoryProvider = Provider((ref){
-  return ActorRepositoryImpl(ActorMovieDbDatasource() );
-
-});
