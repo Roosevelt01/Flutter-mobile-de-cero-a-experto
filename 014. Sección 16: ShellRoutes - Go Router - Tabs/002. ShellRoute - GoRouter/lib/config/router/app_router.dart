7b25d22a35cb140aff14ceb04c4560474d319603
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-01-24 02:41:51 -0400

    flutter

---
+++ 014. Sección 16: ShellRoutes - Go Router - Tabs/002. ShellRoute - GoRouter/lib/config/router/app_router.dart
@@ -0,0 +1,118 @@
+//Paso 1: Se agrega ShellRoute para manejar la navegacion de las pantallas principales
+/*import 'package:cinemapedia/presentation/screen/movies/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+
+    //Paso 1.1:  Dentro del ShellRoute se agrega el HomeScreen como contenedor de las pantallas principales
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+    ),
+
+
+  ],
+);*/
+
+//Paso 2: Iniciamos la configuracion de las rutas hijas dentro del ShellRoute
+/*import 'package:cinemapedia/presentation/Views/home_views/home_view.dart';
+import 'package:cinemapedia/presentation/screen/movies/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      //Paso 2.1: Se agregan las rutas hijas dentro del ShellRoute
+      routes: [
+        //Ruta para la pantalla de inicio
+        GoRoute(
+          path: '/', // con el path '/' se define la ruta inicial
+          builder: (context, state) => const HomeView(), // Se define la vista que se mostrara
+        ),
+      ],
+    ),
+  ],
+);*/
+
+//Paso 3: Agregamos la ruta para la pantalla de favoritos
+/*import 'package:cinemapedia/presentation/Views/home_views/favorites_view.dart';
+import 'package:cinemapedia/presentation/Views/home_views/home_view.dart';
+import 'package:cinemapedia/presentation/screen/movies/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/', 
+          builder: (context, state) => const HomeView(), 
+        ),
+
+        //Paso 3.1: Ruta para la pantalla de favoritos
+        GoRoute(
+          path: '/', 
+          builder: (context, state) => const FavoritesView(), 
+        ),
+
+      ],
+    ),
+  ],
+);*/
+
+//Paso 4: Agregamos la ruta para la pantalla de detalles de la pelicula
+import 'package:cinemapedia/presentation/Views/home_views/favorites_view.dart';
+import 'package:cinemapedia/presentation/Views/home_views/home_view.dart';
+import 'package:cinemapedia/presentation/screen/screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+
+      routes: [
+        GoRoute(
+          path: '/', 
+          builder: (context, state) => const HomeView(), 
+
+          //Paso 4.1: Ruta para la pantalla de detalles de la pelicula
+          routes: [
+            GoRoute( 
+              path: 'movie/:id', // Ruta dinamica que recibe el ID de la pelicula
+              name: MovieScreen.name, // Nombre de la ruta para facilitar la navegacion
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id'; // Obtener el ID de los parametros de la ruta
+                return MovieScreen(movieId: movieId); // Pasar el ID a la pantalla de detalles
+              },
+            ),
+          ]
+        ),
+        
+
+        GoRoute(
+          path: '/', 
+          builder: (context, state) => const FavoritesView(), 
+        ),
+
+      ],
+    ),
+  ],
+);
