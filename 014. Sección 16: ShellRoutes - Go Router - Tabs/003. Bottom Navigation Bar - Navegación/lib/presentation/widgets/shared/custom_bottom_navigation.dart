bf203552f4323ba8789d30c78a26b762bd2f9f69
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-01-26 12:22:27 -0400

    flutter

--- 014. Sección 16: ShellRoutes - Go Router - Tabs/003. Bottom Navigation Bar - Navegación/lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ 014. Sección 16: ShellRoutes - Go Router - Tabs/003. Bottom Navigation Bar - Navegación/lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -56,7 +56,7 @@ class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
 
   //Paso 2.1: Crear una función para obtener el índice actual según la ruta
-  int currentIndex(BuildContext context) {
+  int GetcurrentIndex(BuildContext context) {
     //Paso 2.2: Obtenemos la ubicación actual (ej: '/favorites') desde el Router
     final String location = GoRouterState.of(context).uri.path;
 
@@ -135,11 +135,90 @@ class CustomBottomNavigation extends StatelessWidget {
     }
   }
 
+  int GetcurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.path;
+
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0, 
-      onTap:(value) => onItemTapped(context, value), //Paso 2.1: Llamar a la función onItemTapped
+      onTap:(value) => onItemTapped(context, value), 
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_max),
+          label: 'Inicio',
+        ),
+        
+        BottomNavigationBarItem(
+          icon: Icon(Icons.label_outline),
+          label: 'Categorías', 
+        ),
+        
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_outline),
+          label: 'Favoritos',
+        ),
+      ],
+    );
+  }
+}
+
+//Paso 3: Integración en el build
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomBottomNavigation extends StatelessWidget {
+  const CustomBottomNavigation({super.key});
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0: 
+        context.go('/');
+      break;
+      case 1:
+        context.go('/');
+      break;
+      case 2:
+        context.go('/favorites'); 
+      break;
+    }
+  }
+
+  int GetcurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.path;
+
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      elevation: 0, 
+      currentIndex: currentIndex, //Paso 3.1: Usamos el índice calculado
+      onTap:(value) => onItemTapped(context, value), 
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home_max),
