44f769ff514da9f7af134b6e51eb5ddc5dec8410
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-01-26 11:54:13 -0400

    flutter

--- 014. Sección 16: ShellRoutes - Go Router - Tabs/003. Bottom Navigation Bar - Navegación/lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ 014. Sección 16: ShellRoutes - Go Router - Tabs/003. Bottom Navigation Bar - Navegación/lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -48,19 +48,19 @@ class CustomBottomNavigation extends StatelessWidget {
   }
 }
 
-//Paso 3: Creamos currentIndex para resaltar el item seleccionado(Código final)
+//Paso 2: Creamos currentIndex para resaltar el item seleccionado(Código final)
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
 
-  //Paso 3.1: Crear una función para obtener el índice actual según la ruta
+  //Paso 2.1: Crear una función para obtener el índice actual según la ruta
   int currentIndex(BuildContext context) {
-    //Paso 3.2: Obtenemos la ubicación actual (ej: '/favorites') desde el Router
+    //Paso 2.2: Obtenemos la ubicación actual (ej: '/favorites') desde el Router
     final String location = GoRouterState.of(context).uri.path;
 
-  //Paso 3.3: Usar un switch para retornar el índice según la ruta
+  //Paso 2.3: Usar un switch para retornar el índice según la ruta
     switch (location) {
       case '/':
         return 0;
@@ -92,7 +92,7 @@ class CustomBottomNavigation extends StatelessWidget {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0, 
-      currentIndex: currentIndex(context), //Paso 3.4: Asignar el índice actual al BottomNavigationBar   
+      currentIndex: currentIndex(context), //Paso 2.4: Asignar el índice actual al BottomNavigationBar   
       onTap:(value) => onItemTapped(context, value),
       items: const [
         BottomNavigationBarItem(
